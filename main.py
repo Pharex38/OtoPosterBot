def4624c61b013ed5240ce966eaaa9d6014fd67f
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-03-27 21:38:56 +0300

    update main.py

--- main.py
+++ main.py
@@ -60,10 +60,11 @@ ALL_ROWS = CURSOR.fetchall()
 def gender(update: Update, _: CallbackContext) -> int:
     mesaj = update.message.text
     user = update.message.from_user
-    f = open(f"/{user.id}.txt", "w+")
-    f.write(mesaj)
+    open(f"txtler/{user.id}.txt", "w+").write(mesaj)
+    dosya = f"/{user.id}.txt"
+    dosyakonum = f"{user.id}.txt
     update.message.reply_text(f'*API Kaydedildi. Kısaltmam için bana bir link gönder.* _Tekrar girmek istersen_ /token _yazmanız yeterli._', parse_mode=ParseMode.MARKDOWN)
-    dbx.files_upload(f, dosyakonum, mode =dropbox.files.WriteMode.overwrite)
+    dbx.files_upload(dosya, dosyakonum, mode =dropbox.files.WriteMode.overwrite)
     
     return ConversationHandler.END
 
