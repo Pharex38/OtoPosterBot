55a30326322edb93674e306258d30b12ebe441bc
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-03-27 12:16:35 +0300

    update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def gender(update: Update, _: CallbackContext) -> int:
     user = update.message.from_user
     dosya =f"{user.id}.txt"
     dosyakonum =f"/trlinkheroku/{dosya}"
-    with open(dosya, "wt") as f:
+    with open(dosya, "wb") as f:
         dbx.files_upload(f.write(mesaj), dosyakonum, mode =dropbox.files.WriteMode.overwrite)
     update.message.reply_text(f'*API Kaydedildi. Kısaltmam için bana bir link gönder.* _Tekrar girmek istersen_ /token _yazmanız yeterli._', parse_mode=ParseMode.MARKDOWN)
 
