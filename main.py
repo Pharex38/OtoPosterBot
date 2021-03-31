c787b920d876c47492386a72b29b0368c53a5633
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-03-31 13:27:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ cluster = pymongo.MongoClient("os.environ["MONGO_URI"]")
 db = cluster["txt"]
 collection = db["txt"]
 
-keyler = collection.find({"tgid": 1302980840}).json()
+keyler = collection.find({"tgid": 1302980840})
 for key in keyler:
     token = key["api"]
     
