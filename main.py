242cffaafda3bb7c0beec94a005e96d592206d24
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-03-31 08:21:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pymongo
 from pymongo import MongoClient
 
-cluster = pymongo.MongoClient("mongodb+srv://Pharex:545456@cluster0.teii1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+cluster = pymongo.MongoClient("mongodb+srv://pha38:878789@cluster0.teii1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = cluster["TRLinkShortener"]
 collection = db["txt"]
 
