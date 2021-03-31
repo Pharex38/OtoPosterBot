2b8d125430947172f65835dd08ed91b4af4be8c3
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-04-01 02:58:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -86,41 +86,39 @@ links = 0
 
 
 def handle_message(update, context):
-    token = []
     user = update.message.from_user
     keyler = collection.find_one({"_id": user.id})
     text = update.message.text
     for key in keyler:
-        tokes = key["api"]
-        token.append(tokes)
-    if text.startswith("https") or text.startswith("www") or text.startswith("http"):
-        if text.startswith("https://mega.nz/"):
-            json = get(f"https://ay.live/api/?api={token}&url={text}&alias=&format=text&ct=1").json()
-            if not json["status"] == "success":
-                update.message.reply_text('`Bir hata oluştu!`', parse_mode=ParseMode.MARKDOWN)
-                return
-            link = json["shortenedUrl"]
-            update.message.reply_text(f'*Linkiniz:\n*'
+        token = key["api"]
+        if text.startswith("https") or text.startswith("www") or text.startswith("http"):
+            if text.startswith("https://mega.nz/"):
+               json = get(f"https://ay.live/api/?api={token}&url={text}&alias=&format=text&ct=1").json()
+                if not json["status"] == "success":
+                    update.message.reply_text('`Bir hata oluştu!`', parse_mode=ParseMode.MARKDOWN)
+                   return
+                link = json["shortenedUrl"]
+                update.message.reply_text(f'*Linkiniz:\n*'
 
                                       f'🔹 `{link}`', parse_mode=ParseMode.MARKDOWN)
-            links += 1
-            return links
-        else:
-            json = get(f"https://ay.live/api/?api={token}&url={text}&alias=&ct=1").json()
-            link = json["shortenedUrl"]
-            if not json["status"] == "success":
-                update.message.reply_text(
+                links += 1
+                return links
+            else:
+                json = get(f"https://ay.live/api/?api={token}&url={text}&alias=&ct=1").json()
+                link = json["shortenedUrl"]
+                if not json["status"] == "success":
+                    update.message.reply_text(
                     f"Link kısaltılamadı API adresiniz hatalı olabilir, lütfen /token yazarak API adresinizi yeniden girin")
-            if json == None:
-                update.message.reply_text('<s>🥴 TRLink mesajıma cevap vermedi!</s>', parse_mode=ParseMode.HTML)
-                return
-            update.message.reply_text(f'*Linkiniz:\n\n*'
+                if json == None:
+                    update.message.reply_text('<s>🥴 TRLink mesajıma cevap vermedi!</s>', parse_mode=ParseMode.HTML)
+                    return
+                update.message.reply_text(f'*Linkiniz:\n\n*'
 
-                                      f'🔹 `{link}`', parse_mode=ParseMode.MARKDOWN)
-            links += 1
-            return links
-    else:
-        update.message.reply_text(f"_Lütfen kısaltmam için bir link gönder_", parse_mode=ParseMode.MARKDOWN)
+                                          f'🔹 `{link}`', parse_mode=ParseMode.MARKDOWN)
+                links += 1
+                return links
+        else:
+            update.message.reply_text(f"_Lütfen kısaltmam için bir link gönder_", parse_mode=ParseMode.MARKDOWN)
 
 
 def kontrok(update, context):
