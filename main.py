ced9e045e67ed7c08c08f3c93682947ffaa99e8f
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-04-01 00:53:42 +0300

    _id

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ ALL_ROWS = CURSOR.fetchall()
 def gender(update: Update, _: CallbackContext) -> int:
     mesaj = update.message.text
     user = update.message.from_user
-    key = {"tgid": f"{user.id}", "api": f"{mesaj}"}
+    key = {"_id": user.id, "api": f"{mesaj}"}
     collection.insert_one(key)
     update.message.reply_text(f'*API Kaydedildi. Kısaltmam için bana bir link gönder.* _Tekrar girmek istersen_ /token _yazmanız yeterli._', parse_mode=ParseMode.MARKDOWN)
 
@@ -88,7 +88,7 @@ links = 0
 def handle_message(update, context):
     user = update.message.from_user
     try:
-        keyler = collection.find({"tgid": f"{user.id}"}).json()
+        keyler = collection.find({"tgid": user.id})
         for key in keyler:
             token = key["api"]
     except:
