7dfd3b856d28bce930e48e10f3fa0a3d1ad330c0
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-03-31 16:30:26 +0300

    update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ links = 0
 
 def handle_message(update, context):
     user = update.message.from_user
-    keyler = collection.find({"tgid": f"{user.id}"}).json()
+    keyler = collection.find({"tgid": f"{user.id}"})
     for key in keyler:
         token = key["api"]
         text = str(update.message.text)
