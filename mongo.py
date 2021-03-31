6f33bb95eb8ecdbf3cc732b92eee23feb6cc2dc0
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-03-31 13:12:36 +0300

    ğ
    0

---
+++ mongo.py
@@ -0,0 +1,12 @@
+import pymongo
+from pymongo import MongoClient
+
+cluster = pymongo.MongoClient("mongodb+srv://Pharex:545456@cluster0.teii1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = cluster["txt"]
+collection = db["txt"]
+
+keyler = collection.find({"tgid": f"{user.id}"}).json()
+for key in keyler:
+    token = key["api"]
+
+print(token)
