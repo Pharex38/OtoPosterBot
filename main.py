17d7d36ec0d50b0790888c72c097f9092f1e802a
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-04-02 12:29:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ collection = db["txt"]
 
 cursor = collection.find_one({"_id": 1302980840})
 token = cursor['api']
-text = "www.yandex.com"
-link = get(f"https://ay.live/api/?api={token}&url={text}&alias=&ct=1").json()
+print(token)
+link = get(f"https://ay.live/api/?api={token}&url=https://www.google.com&alias=&ct=1").json()
 mesaj = link['shortenedUrl']
 
-print(token)
 print(mesaj)
