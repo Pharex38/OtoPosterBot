0895619715d38e53f7c1ecd063d2eee5c5c5dac7
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-04-02 13:11:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ cursor = collection.find_one({"_id": 1302980840})
 token = cursor['api']
 text = "www.google.com"
 link = get(f"https://ay.live/api/?api={token}&url={text}&alias=&ct=1").json
-
-print(link['shortenedUrl'])
+top = link['shortenedUrl']
+print(top)
