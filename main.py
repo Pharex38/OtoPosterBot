483e68be94719e7e858b10ff6185d954666d9b5e
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-06 00:47:49 +0300

    update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def start(m):
     chat = m.chat.id
     msg = m.reply_to_message.message_id
     print(msg)
-    while len(msg) < 100:
+    while len(int(msg)) < 100:
         msg += 1
         mesajlar.append(msg)
         bot.delete_message(chat, msg)
