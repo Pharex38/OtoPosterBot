fa901c91c1a7d21efe61f7fc208d48563441cbb4
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-07 06:51:21 +0300

    update main.py

--- main.py
+++ main.py
@@ -64,8 +64,8 @@ def temizlik(m):
         else:
             sayi += 1
         
-    bot.send_message(chat, f"{sayi} adet mesaj temizlendi.")
-    bot.send_message(-1001391561285, f"{sayi} adet mesaj silindi.\n\n Kanal: {chat})
+    bot.send_message(chat, f"`{sayi}` *adet mesaj temizlendi.*", parse_mode=ParseMode.MARKDOWN)
+    bot.send_message(-1001391561285, f"`{sayi}` *adet mesaj silindi.*\n\n *anall:* `{chat}`", parse_mode=ParseMode.MARKDOWN)
 
 
 bot.polling()
