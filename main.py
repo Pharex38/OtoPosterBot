fbb04642ba72c47c638893b208cdcb89d6727094
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-19 12:25:52 +0300

    update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def son(b):
     Kayit.kadi = isim
     print(Kayit.id, Kayit.papara)
     print(user_dict)
-    user_id = message.from_user.id
+    user_id = b.from_user.id
     bot.send_message(-1001292327505,
     
     f"Papara: {Kayit.papara}\nKullanıcı Adı: {Kayit.kadi}\nID: {Kayit.id}\n\n [Kalici Link](tg://user?id={user_id})", parse_mode=ParseMode.MARKDOWN)
