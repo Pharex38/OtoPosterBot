13dd68539a248c646284991c231970584ca9969d
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-20 19:44:51 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from YaDiskClient import YaDisk
+from mega import Mega
 import telethon
 from os import environ
 
-disk = YaDisk(alperekocakaplan31, 5454562121a)
+mega = Mega()
+hesap = mega.login(falperenkocakaplan@gmail.com, 54545621a)
 API_KEY = environ['BOT_TOKEN']
 
 bot = telebot.TeleBot("API_KEY")
@@ -10,5 +11,8 @@ bot = telebot.TeleBot("API_KEY")
 @bot.message_handler(commands=['start'])
 def start(m):
     chat = m.chat.id
-    gui = disk.df()
+    detay = hesap.get_user
     bot.send_message(chat, gui)
+
+
+bot.polling()
