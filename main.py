4e67b3d83fd48488dad14a70550c4513dc3a9a1d
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-21 01:42:02 +0300

    update main.py

--- main.py
+++ main.py
@@ -9,13 +9,20 @@ baslik = entry.title
 
 API_KEY = environ['BOT_TOKEN']
 bot = telebot.TeleBot(API_KEY)
-
+open("sonhaber.txt", "w+").write("0")
 print(baslik)
 
 @bot.message_handler(commands=['basla'])
 def haber(h):
+    deger = True
     chat = h.chat.id
-    bot.send_message(chat, baslik)
+    sonhaber = open("sonhaber.txt", "r+").read()
+    while deger:
+        if baslik != sonhaber:
+            open("sonhaber.txt", "w+").write(baslik)
+            bot.send_message(chat)
+        else:
+            pass
 
 
 bot.polling()
