faf474ba4206464fd2e912ae9228de47db0b7845
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-21 01:16:15 +0300

    update main.py

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ afeed = feedparser.parse("https://www.aa.com.tr/tr/rss/default?cat=guncel")
 haber = afeed.keys
 entry = afeed.entries[1]
 
-print entry.keys()
+print(entry.keys())
+print(entry.title())
