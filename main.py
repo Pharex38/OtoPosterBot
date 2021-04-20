75bc91d9d0e33849d33b81feda36d2b12aea7d3e
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-21 00:33:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from os import environ
 
 mega = Mega()
 hesap = mega.login(falperenkocakaplan@gmail.com, 54545621a)
-API_KEY = environ['BOT_TOKEN']
+API_KEY = "***REMOVED-BOT-TOKEN***"
 
 bot = telebot.TeleBot("API_KEY")
 
