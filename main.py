a14bfe4afbdd408012f70e177727ff8d76e668b8
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-20 19:26:35 +0300

    update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from YaDiskClient import YaDisk
 import telethon
 from os import environ
 
-disk = YaDiskClient.YaDisk(alperekocakaplan31, 5454562121a)
+disk = YaDisk(alperekocakaplan31, 5454562121a)
 API_KEY = environ['BOT_TOKEN']
 
 bot = telebot.TeleBot("API_KEY")
