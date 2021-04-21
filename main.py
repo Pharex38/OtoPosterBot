9908a4d0a12d40143c9b9327326a587c5226a23e
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-21 11:45:13 +0300

    update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ api_hash = "ddd27540833843f8196318096eeaff5e"
 
 app = Client("my_account", api_id, api_hash)
 print("Başlıyor")
+app.start()
 
 app.send_message("me", "Greetings from **Pyrogram**!")
 
