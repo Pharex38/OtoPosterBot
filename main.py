9323cc9746deafb4bd7652efa98b2a33c664ed25
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-21 13:12:10 +0300

    update main.py

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ app = Client("my_account", api_id, api_hash)
 print("Başlıyor")
 app.start()
 
-
-app.send_message("me", "Greetings from **Pyrogram**!")
-
-@app.on_message()
-def my_handler(client, message):
-    message.forward("me")
+@app.on_message(filters.text & filters.private)
+def echo(client, message):
+    message.reply_text(message.text)
 
 
+app.run()  # Automatically start() and idle()
