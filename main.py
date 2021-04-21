f724eef92a663ab7b50befd610adf1487d33a303
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-21 13:16:32 +0300

    update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ api_hash = "ddd27540833843f8196318096eeaff5e"
 app = Client("my_account", api_id, api_hash)
 print("Başlıyor")
 
-@app.on_message(filters.text & filters.private)
+@app.on_message(filters.text & "-1001204665413")
 def echo(client, message):
     message.reply_text(message.text)
 
