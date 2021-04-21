37e8f8664ac1ba79d443d689142f807c661026cc
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-21 13:29:36 +0300

    update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ api_hash = "***REMOVED-API-HASH***"
 app = Client("my_account", api_id, api_hash)
 print("Başlıyor")
 
-@app.on_message(-1001204665413)
+@app.on_message(filters.channel & -1001204665413)
 def echo(client, message):
-    message.reply_text(message.text)
+    app.send_message(message.text)
 
 
 app.run()  # Automatically start() and idle()
