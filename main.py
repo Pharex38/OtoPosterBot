7e309ddc255a963cce7e112883356387e748f2ac
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-21 13:39:09 +0300

    update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ api_hash = "***REMOVED-API-HASH***"
 app = Client("my_account", api_id, api_hash)
 print("Başlıyor")
 
-@app.on_message(filters.channel & filters.create(lambda c,m: m.chat.id == -1001204665413))
+@app.on_message(filters.text & filters.channel)
 def echo(client, message):
     app.send_message("-1001196621427", message.text)
 
