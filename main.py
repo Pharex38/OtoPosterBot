ad70376c2aff1e59f20e7ed84bb2a227310804b9
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-21 12:43:53 +0300

    update main.py

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ app = Client("my_account", api_id, api_hash)
 print("Başlıyor")
 
 
+
 app.send_message("me", "Greetings from **Pyrogram**!")
+
+def my_function(client, message):
+    message.forward("me")
 
 my_handler = MessageHandler(my_function)
 app.add_handler(my_handler)
