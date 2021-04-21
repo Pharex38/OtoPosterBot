fcfe2e952742a0f817e24852363956b2b42cb3ef
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-21 11:42:21 +0300

    update main.py

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ api_id = ***REMOVED-API-ID***
 api_hash = "***REMOVED-API-HASH***"
 
 app = Client("my_account", api_id, api_hash)
+print("Başlıyor")
 
 app.send_message("me", "Greetings from **Pyrogram**!")
+
