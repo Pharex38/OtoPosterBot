8e0682defb3483d31ac20ec42692222128f5357b
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-22 10:11:24 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
 from mega import Mega
 from os import environ
+import requests
 
 mega = Mega()
 email = "falperenkocakaplan@gmail.com"
@@ -12,6 +13,9 @@ print(details)
 API_KEY = environ['BOT_TOKEN']
 bot = telebot.TeleBot(API_KEY)
 
+den = m.download_url("https://mega.nz/file/DfoxyArC#jfgaNkO-AnKY69c59UtP5rw24treol-hmcq4JzPPGew")
+print(den)
+
 @bot.message_handler(commands=['start'])
 def start(s):
     email = details['email']
@@ -19,5 +23,14 @@ def start(s):
     chat = s.chat.id
     bot.send_message(chat, f"Mail: {email}\nİsim: {isim}")
 
+@bot.message_handler(func=lambda message: True, content_types=["text"])
+def dosya(d):
+    chat = d.chat.id
+    mesaj = d.text
+    if mesaj.startswith("https://mega"):
+        dosya = get(mesaj)
+        
+        m.download_url(link)
+    
 
 bot.polling()
