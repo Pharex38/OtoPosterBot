61112756bee50c7a51670729b7a91719963801ea
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-22 12:00:19 +0300

    update main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def start(s):
     isim = details['name']
     chat = s.chat.id
     bot.send_message(chat, f"Mail: {email}\nİsim: {isim}")
+    #
 
 @bot.message_handler(func=lambda message: True, content_types=["text"])
 def dosya(d):
