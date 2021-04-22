5714dda9cbd7c011ec47ee59d095f65741aef780
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-22 10:32:43 +0300

    update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ API_KEY = environ['BOT_TOKEN']
 bot = telebot.TeleBot(API_KEY)
 
 den = m.download_url("https://mega.nz/file/DfoxyArC#jfgaNkO-AnKY69c59UtP5rw24treol-hmcq4JzPPGew")
-indir = m.download(den, '/Dosyalar', den)
-print(indir)
+#indir = m.download(den, '/Dosyalar', den)
+print(den)
 
 @bot.message_handler(commands=['start'])
 def start(s):
