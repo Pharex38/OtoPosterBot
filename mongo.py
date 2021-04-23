5f3151508a78c918b0af789f9c37b02670404414
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-23 17:57:24 +0300

    update mongo.py

--- mongo.py
+++ mongo.py
@@ -14,6 +14,14 @@ API_KEY = environ['BOT_TOKEN']
 bot = telebot.TeleBot(API_KEY)
 
 
+@app.on_message(filters.command(['oynat']))
+def oynat(client, o):
+    chat = o.chat.id
+    komut = subprocess.call(f'ffmpeg -y -i http://stream2.taksimbilisim.com:8010/ -f s16le -ac 1 -acodec pcm_s16le -ar 128 {yol}/yayin.raw', shell=True)
+    print(komut)
+    app.send_message(chat, "Oynatılıyor.")
+    pytgcalls.join_group_call(-1001391561285, 'yayin.raw')
+    app.send_message(chat, "Çalıyor.")
 
 @bot.message_handler(commands=['start'])
 def start(s):
