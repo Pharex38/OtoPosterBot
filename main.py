0b8ff7ff2ced53ed0a2fb4ae35f4fab01aa0eaba
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-23 19:37:29 +0300

    update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def hesap(client, message):
     isim = details['name']
     print(dosyalar)
     print(details)
-    app.send_message(cid, f"**Hesap Bilgileriniz;**\n\nİsim: {isim} "\nE-mail: {details['email']}\nBulut Alanı: {alan}\nQuota: {quota}")
+    app.send_message(cid, f"**Hesap Bilgileriniz;**\n\nİsim: {isim} \nE-mail: {details['email']} \nBulut Alanı: {alan}\nQuota: {quota}")
 
 
 @app.on_message(filters.document)
