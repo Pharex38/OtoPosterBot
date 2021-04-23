c66106aa718333eacad0eee41b7e9fad2fc94e4f
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-23 22:55:57 +0300

    update main.py

--- main.py
+++ main.py
@@ -62,10 +62,15 @@ def giris(client, message):
 
 @app.on_message(filters.command(['hesap']))
 def hesap(client, message):
+    user = message.from_user.id
     cid = message.chat.id
     mid = message.message_id
     mids = mid+1
     app.send_message(cid, "`Yükleniyor...`")
+    hbilgi = collection.find_one({"_id": user})
+    email = hbilgi['email']
+    sifre = hbilgi['sifre']
+    m = mega.login(email, sifre)
     bulut = m.get_storage_space(giga=True)
     alan = round(bulut['used'],2)
     quota = m.get_quota()
@@ -81,18 +86,25 @@ def hesap(client, message):
 def dosya(client, message):
     global chat
     global mids
-    sayi = 0
+    user = message.from_user.id
     chat = message.chat.id
     mid = message.message_id
     mids = mid+1
     app.send_message(chat, "Dosya indirliyor...")
     indirilen = message.download(progress=progress)
     app.edit_message_text(chat, mids, "`Dosya Mega'ya yükleniyor...`")
-    yuklenen = m.upload(indirilen)
-    print(yuklenen)
-    link = m.get_upload_link(yuklenen)
-    print(link)
-    app.send_message(chat, f"**İşlem Tamamlandı:** \n\nLinkiniz: {link}")
+    hbilgi = collection.find_one({"_id": user})
+    if hbilgi == None:
+        app.send_message(chat, "Bu özelliği kullanabilmek için önce giriş yapmanız gerekiyor. /giris komutu ile giriş yapabilirsiniz.")
+    else:
+        email = hbilgi['email']
+        sifre = hbilgi['sifre']
+        m = mega.login(email, sifre)
+        yuklenen = m.upload(indirilen)
+        print(yuklenen)
+        link = m.get_upload_link(yuklenen)
+        print(link)
+        app.send_message(chat, f"**İşlem Tamamlandı:** \n\nLinkiniz: {link}")
 
 
 app.run()
