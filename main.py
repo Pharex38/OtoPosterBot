1b15fc3b773a22fc8f14cb9eddb021e908f92da4
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-23 03:35:56 +0300

    update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def echo(client, message):
 @app.on_message(filters.command(['oynat']))
 def oynat(client, o):
     chat = o.chat.id
-    subprocess.call('ffmpeg -y -i http://stream2.taksimbilisim.com:8010/ -f s16le -ac 1 -acodec pcm_s16le -ar 128 yayin.raw')
+    subprocess.call('ffmpeg -y -i http://stream2.taksimbilisim.com:8010/ -f s16le -ac 1 -acodec pcm_s16le -ar 128 /yayin.raw')
     pytgcalls.join_group_call(-1001391561285, 'yayin.raw')
 
 
