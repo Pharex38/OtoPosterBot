340dca4139167545f6b2a84dbe0eec0b04366980
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-23 11:38:17 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pyrogram import *
 from pyrogram.handlers import MessageHandler
 from os import environ
+import os
 import subprocess
 from pytgcalls import *
 
@@ -11,7 +12,7 @@ app = Client("RadyoBot", api_id, api_hash, bot_token=BOT_TOKEN)
 pytgcalls = PyTgCalls(app, log_mode=PyLogs.verbose)
 print("Başlıyor")
 
-komut = subprocess.call('ffmpeg -y -i http://yayin.yayindakiler.com:4010/;stream.mp3 -f s16le -ac 1 -acodec pcm_s16le -ar 144 yayin.mp3', shell=True)
+komut = subprocess.call('ffmpeg -y -i http://stream2.taksimbilisim.com:8010/ -f s16le -ac 1 -acodec pcm_s16le -ar 128 /yayin.raw', shell=True)
 print(komut)
 
 
@@ -29,4 +30,4 @@ def oynat(client, o):
 
 
 
-app.run()
+app.run()  # Automatically start() and idle()
