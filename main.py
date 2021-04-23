ab1a8f282bf66f014b06bb9bf1e822fbb0264629
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-23 03:49:18 +0300

    update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pyrogram import *
 from pyrogram.handlers import MessageHandler
 from os import environ
 import subprocess
+import PyTgCalls
 
 api_id = ***REMOVED-API-ID***
 api_hash = "***REMOVED-API-HASH***"
