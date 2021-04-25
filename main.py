2730905af18542d261fc4f034731c2e4858af1ca
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-26 01:38:07 +0300

    update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from pyrogram import Client
 
 api_id = 3489775
 api_hash = "ddd27540833843f8196318096eeaff5e"
-#app = Client( s, api_id, api_hash)
+botapi = "1718645974:AAHbjtjhWcRn0RZfhyrsKB-1hO4jYpNDgoQ"
+bot = Client( s, api_id, api_hash, bot_token=botapi)
 print("Başlıyor")
-#app.start()
 
 metin = "Eve attıkları kızlara arkalı önlü döşüyorlar https://streamtape.com/e/1dM4xp48LQseqe8"
 metin = metin.split("http")
@@ -12,5 +12,11 @@ print(metin)
 metin = metin[1]
 print(metin)
 
-#app.send_message("me", "Greetings from **Pyrogram**!")
+@bot.on_message(filters.private)
+def post(client, message):
+    mesaj = message.text
+    print(mesaj)
 
+
+
+bot.run()
