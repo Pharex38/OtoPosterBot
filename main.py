bf6d801f19e489c5a9c099ede99b88d4171d1cd4
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-26 01:59:34 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pyrogram
 from pyrogram import *
+import requests
+from os import environ
 
 api_id = ***REMOVED-API-ID***
 api_hash = "***REMOVED-API-HASH***"
@@ -7,17 +9,12 @@ botapi = "***REMOVED-BOT-TOKEN***"
 bot = Client("bot", api_id, api_hash, bot_token=botapi)
 print("Başlıyor")
 
-metin = "Eve attıkları kızlara arkalı önlü döşüyorlar https://streamtape.com/e/1dM4xp48LQseqe8"
-metin = metin.split("http")
-print(metin)
-metin = metin[1]
-print(metin)
-
 @bot.on_message(~filters.channel)
 def post(client, message):
     chat = message.chat.id
     mesaj = message.caption
-    print(mesaj)
+    medya = message.photo
+    print(medya)
     mesaj = mesaj.split("http")
     mesaj = mesaj[1]
     bot.send_message(chat, f"http{mesaj}")
