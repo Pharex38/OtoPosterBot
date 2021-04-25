7b0c050257388e479f58e2759c69209dfbad228f
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-26 01:19:00 +0300

    update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from pyrogram import Client
 
 api_id = 3489775
 api_hash = "ddd27540833843f8196318096eeaff5e"
-strings = 
-app = Client( strings, api_id, api_hash)
+app#app = Client( s, api_id, api_hash)
 print("Başlıyor")
 app.start()
 
