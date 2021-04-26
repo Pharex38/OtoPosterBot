2a72858bf922d5118fdcac792b218ceceaacec30
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-26 17:27:25 +0300

    update main.py

--- main.py
+++ main.py
@@ -50,7 +50,8 @@ def post(client, message):
                 link = json['shortenedUrl']
                 try:
                     bot.send_photo(kanal, medya, caption=f"{mesaj[0]}KTE: {link}\n\n {mesaja[1]}\n\n{mesaja[2]}{mesaja[3]}")
-                except:
+                except Exception as e:
+                    print(e)
                     pass
         else:
             for hesap in binb:
