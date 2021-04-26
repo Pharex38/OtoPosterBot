24c31b433028cd6ab49cf5d4e6f8ab66b70daaf7
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-26 19:19:51 +0300

    update main.py

--- main.py
+++ main.py
@@ -27,12 +27,12 @@ print("Başlıyor")
     
 @bot.on_message(filters.command(['start']))
 def start(client, message):
-    chat = message.chat.id
+    chat = message.chat_id
     bot.send_message(chat, "Merhaba!\n\n**Ne İşe Yarıyor?**\nBu bot [Link Mahzeni'nde](https://t.me/joinchat/UYu8q0gBTUdUudDL) paylaşılan postların linklerini otomatik olarak kısaltıp sizin kanalınıza iletir.\n\n**Nasıl Kullanılır?**\n1. Adım: Botu kanlınıza yönetici olarak ekleyin. \n2. Adım: API adresinizi ve Kanal ID girin. \n-/kaydet TRLINK_API KANAL_ID \n3. Adım: Kanalınzda /onayla yazın.\n4. Adım: Keyfini çıkarın.\n\n__NOT: Kanalınızın ID numarasını bilmiyorsanız kanaldan bota bir post iletin bot size söyleyecek.__", disable_web_page_preview=True)
 
-@bot.on_message(filters.channel)
-def post(client, message):
-    chat = message.chat.id
+@bot.on(events.NewMessage(pattern='(?i).*/start'))
+async def post(message):
+    chat = message.chat_id
     print(chat)
     """if chat == -1001368112299 or chat == -1001352123979:
         mesaj = message.caption
@@ -74,27 +74,26 @@ def post(client, message):
                     print(e)
                     print(kanal)
         bot.send_message(chat, "Başarılı!")"""
-    if message.text == "/onayla":
-        new_rights = ChatAdminRights(post_messages=True, add_admins=True,
+    new_rights = ChatAdminRights(post_messages=True, add_admins=True,
                                  invite_users=True,
                                  change_info=True,
                                  ban_users=True,
                                  delete_messages=True)
     
-        yetki = bot.send(functions.channels.EditAdmin(channel=chat, user_id=1671239079, admin_rights=new_rights, rank="otopost"))
-        print(yetki)
-        bot.send_message(chat, "Tamamdır!")
+    await message.client(EditAdminRequest(chat, 1671239079, new_rights, "Post"))
+    print(yetki)
+    await bot.send_message(chat, "Tamamdır!")
 
-@bot.on_message(filters.private)
-def aydialma(client, message):
+@bot.on(events.NewMessage)
+async def aydialma(client, message):
     print(message.text)
     if not message.text == None and message.text.startswith("/kaydet"):
         mesaj = message.text.split(None, 2)[1:]
-        cid = message.chat.id
+        cid = message.chat_id
         user = message.from_user.id
         print(mesaj)
         if len(mesaj) < 2:
-            bot.send_message(cid, "Yanlış kullanım! \n\n-/kaydet TRLINK_API KANAL_ID")
+            await bot.send_message(cid, "Yanlış kullanım! \n\n-/kaydet TRLINK_API KANAL_ID")
             return
         key = {"_id": user, "token": mesaj[0], "kanal": mesaj[1]}
         bnb = collection.find_one({"_id": user})
@@ -103,12 +102,12 @@ def aydialma(client, message):
         else:
             collection.update_one({"_id": user}, {"$set":{"token": mesaj[0], "kanal": mesaj[1]}})
     
-        bot.send_message(cid, "Kaydedildi!")
+        await bot.send_message(cid, "Kaydedildi!")
     else:
-        chat = message.chat.id
+        chat = message.chat_id
         ileti = message.forward_from_chat
         ileti = ileti['id']
-        bot.send_message(chat, f"Kanal ID: `{ileti}`")
+        await bot.send_message(chat, f"Kanal ID: `{ileti}`")
 
 
 
