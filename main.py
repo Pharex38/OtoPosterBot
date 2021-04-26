661b428bfce23fa4c2f63dbb6429d1c9925c46b0
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-26 05:10:37 +0300

    update main.py

--- main.py
+++ main.py
@@ -32,8 +32,11 @@ def post(client, message):
     except:
         medya = message.photo
         medya = medya['file_id']
-    for xat in kanallar:
-        bot.send_photo(xat, medya, caption=f"{mesaj[0]}KTE: {link}\n\n {mesaja[1]}\n\n{mesaja[2]}{mesaja[3]}")
+        for xat in kanallar:
+            bot.send_photo(xat, medya, caption=f"{mesaj[0]}KTE: {link}\n\n {mesaja[1]}\n\n{mesaja[2]}{mesaja[3]}")
+    else:
+        for xat in kanallar:
+            bot.send_photo(xat, medya, caption=f"{mesaj[0]}KTE: {link}\n\n {mesaja[1]}\n\n{mesaja[2]}{mesaja[3]}")
     bot.send_message(chat, "Başarılı!")
 
 
