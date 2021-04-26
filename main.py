0200e9d85093d61591be73867d6c7a4e7d36e9b2
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-26 04:58:52 +0300

    update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ api_hash = "227747362d2538b4c7694da4bf04e627"
 botapi = "1718645974:AAHbjtjhWcRn0RZfhyrsKB-1hO4jYpNDgoQ"
 token = "13c86bc3b625bf15995d018810d38737e6e70197"
 bot = Client("bot", api_id, api_hash, bot_token=botapi)
+kanallar = ['-1001352123979', '-1001444935707']
 print("Başlıyor")
 
 @bot.on_message(~filters.group)
@@ -27,7 +28,8 @@ def post(client, message):
     cookies = dict(link.cookies)
     json = s.get(f"https://ay.live/api/?api={token}&url={mesajb}&alias=&ct=1", cookies=cookies).json()
     link = json['shortenedUrl']
-    bot.send_photo(-1001444935707, medya, caption=f"{mesaj[0]}KTE: {link}\n\n {mesaja[1]}\n\n{mesaja[2]}{mesaja[3]}")
+    for xat in kanallar:
+        bot.send_photo(xat, medya, caption=f"{mesaj[0]}KTE: {link}\n\n {mesaja[1]}\n\n{mesaja[2]}{mesaja[3]}")
     bot.send_message(chat, "Başarılı!")
 
 
