4d89d76578c881749299caa9b7566d018df06337
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-26 05:23:30 +0300

    update main.py

--- main.py
+++ main.py
@@ -34,7 +34,10 @@ def post(client, message):
             bot.send_photo(xat, medya, caption=f"{mesaj[0]}KTE: {link}\n\n {mesaja[1]}\n\n{mesaja[2]}{mesaja[3]}")
     else:
         for xat in kanallar:
-            bot.send_video(xat, medya, caption=f"{mesaj[0]}KTE: {link}\n\n {mesaja[1]}\n\n{mesaja[2]}{mesaja[3]}")
+          try:
+              bot.send_video(xat, medya, caption=f"{mesaj[0]}KTE: {link}\n\n {mesaja[1]}\n\n{mesaja[2]}{mesaja[3]}")
+          except:
+              pass
     bot.send_message(chat, "Başarılı!")
 
 
