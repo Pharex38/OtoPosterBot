65b60af7b6e2049c98f95768ac9b85a94135750c
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-26 12:34:46 +0300

    update main.py

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def post(client, message):
         medya = message.photo
         medya = medya['file_id']
         for hesap in binb:
+            print(hesap)
             token = hesap['token']
             kanal = hesap['kanal']
             json = s.get(f"https://ay.live/api/?api={token}&url={mesajb}&alias=&ct=1", cookies=cookies).json()
