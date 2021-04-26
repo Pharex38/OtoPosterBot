8563e8eded581c1feaeedd04a4c060a9336c631f
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-27 00:15:16 +0300

    update main.py

--- main.py
+++ main.py
@@ -50,6 +50,8 @@ async def post(message):
 
 @bot.on(events.NewMessage)
 async def aydialma(message):
+    getter = await client.get_entity(message.chat_id)
+    print(getter
     if not message.text == None and message.text.startswith("/kaydet"):
         mesaj = message.text.split(None, 2)[1:]
         cid = message.chat_id
