64445c3bd55e9b5c359507450fcdf6222bb37ade
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-27 11:40:12 +0300

    update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ mongo = environ['MONGO']
 cluster = MongoClient(mongo)
 db = cluster["OtoPost"]
 collection = db["Kanallar"]
-bot = telebot.TeleBot(botapi, parse_mode=MARKDOWN)
+bot = telebot.TeleBot(botapi, parse_mode=ParseMode.MARKDOWN)
 print("Başlıyor")
     
 @bot.message_handler(commands=['start'])
