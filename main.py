a7b199f6d2c7f41f36863c4067706e59f3352853
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-27 21:28:34 +0300

    update main.py

--- main.py
+++ main.py
@@ -115,8 +115,7 @@ def poster(message):
             kanal = hesap['kanal']
             json = s.get(f"https://ay.live/api/?api={token}&url={mesajb}&alias=&ct=1", cookies=cookies).json()
             link = json['shortenedUrl']
-            print(token)
-            print(kanal)
+            print(f"{kanal} + {link} + {token}")
             try:
                 time.sleep(1)
                 bot.send_photo(kanal, medya, caption=f"{mesaj[0]}KTE: {link}\n\n {mesaja[1]}\n\n{mesaja[2]}{mesaja[3]}")
@@ -149,8 +148,7 @@ def poster(message):
             kanal = hesap['kanal']
             json = s.get(f"https://ay.live/api/?api={token}&url={mesajb}&alias=&ct=1", cookies=cookies).json()
             link = json['shortenedUrl']
-            print(token)
-            print(kanal)
+            print(f"{kanal} + {link} + {token}")
             try:
                 time.sleep(1)
                 bot.send_video(kanal, medya, caption=f"{mesaj[0]}KTE: {link}\n\n {mesaja[1]}\n\n{mesaja[2]}{mesaja[3]}")
