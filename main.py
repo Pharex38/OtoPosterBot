f12c86da823981c66be7a1e76315b8ea6eaa5bbd
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-27 15:11:46 +0300

    update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def start(message):
     chat = message.chat.id
     bot.send_message(chat, "Merhaba!\n\n*Ne İşe Yarıyor?*\nBu bot [Link Mahzeni'nde](https://t.me/joinchat/UYu8q0gBTUdUudDL) paylaşılan postların linklerini otomatik olarak kısaltıp sizin kanalınıza iletir.\n\n*Nasıl Kullanılır?*\n1. Adım: Botu kanlınıza yönetici olarak ekleyin. \n2. Adım: API adresinizi ve Kanal ID girin. \n-/kaydet TRLINK_API KANAL_ID \n3. Adım: Kanalınzda /onayla yazın.\n4. Adım: Keyfini çıkarın.\n\n_NOT: Kanalınızın ID numarasını bilmiyorsanız kanaldan bota bir post iletin bot size söyleyecek._", parse_mode='MarkDown', disable_web_page_preview=True)
 
-@bot.message_handler(commands=['durdur'])
+@bot.message_handler(commands=['sil'])
 def durdur(message):
     chat = message.chat.id
     user = message.from_user.id
