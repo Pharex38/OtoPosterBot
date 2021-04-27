4668f3eb72b9cc8ccab2e76b21feb4571fa22adf
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-27 11:54:57 +0300

    update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def aydialma(message):
         bot.send_message(cid, "Kaydedildi!")
     elif message.chat.type == "private":
         chat = message.chat.id
-        ileti = str(message.from_user)
+        ileti = str(message.chat)
        # ileti = ileti.replace("PeerChannel(channel_id=", "").replace(")", "")
         
         bot.send_message(chat, f"Kanal ID: `-100{ileti}`")
