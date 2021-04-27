436ac68d4f31ddc24c4d0a1fef882c279fa728a3
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-27 18:39:31 +0300

    update main.py

--- main.py
+++ main.py
@@ -60,9 +60,9 @@ def apikayit(message):
     mid = message.id
     mids = mid+1
     chat = message.chat.id
-    bot.send_message(chat, "`API adresiniz kontrol ediliyor...`")
-    key = get(f"https://tr.link/api/?api={token}&url=yourdestinationlink.com&alias=&format=text&ct=1").text
-    if key == "":
+    bot.send_message(chat, "`API adresiniz kontrol ediliyor...`", parse_mode='MarkDown')
+    ket = get(f"https://tr.link/api/?api={token}&url=yourdestinationlink.com&alias=&format=text&ct=1").text
+    if ket == "":
         msg = bot.edit_message_text(chat, mids, "Geçersiz Bir API adresi girdiniz! Lütfen [bu adresten](https://tr.link/member/tools/api) yeniden alın.", parse_mode='MarkDown')
         bot.register_next_step_handler(msg, apikayit)
         return
