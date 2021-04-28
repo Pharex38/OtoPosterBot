0219c1bb6307bce0e9565ad97908570ea52c52cd
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-28 03:45:23 +0300

    update main.py

--- main.py
+++ main.py
@@ -155,9 +155,9 @@ def poster(message):
             link = json['shortenedUrl']
             print(f"{kanal} + {link} + {token}")
             try:
-                if sablon == 0:
+                if sablon == 1:
                     sablon = f"🔥{aciklama}\n\n🔱 TIKLA 👉 {link}\n\n📛 SESİ AÇ 'a tıklamayı unutma"
-                elif sablon == 1:
+                elif sablon == 2:
                     sablon = f"🔴 {aciklama}\n\n❌ SILINMEDEN IZLE ❌\n\n👉 DEVAMI LİNKTE: {link}\n\n🔔ʙɪʟᴅɪʀɪᴍʟᴇʀɪ ᴀçᴍᴀʏı ᴜɴᴜᴛᴍᴀʏıɴ.\n\n📌 Link Nasıl Açılır Bilmiyorsanız\n\n👉 @linkgecmeth"
                 
                 sleep(1)
