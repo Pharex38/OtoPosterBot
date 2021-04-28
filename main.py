f51b03ef6d7585d855100b45b4ed8d2ce4348c41
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-28 14:04:32 +0300

    update main.py

--- main.py
+++ main.py
@@ -199,10 +199,15 @@ def poster(message):
             try:
                 if sablon == "1":
                     sablon = f"🔥{aciklama}\n\n🔱 TIKLA 👉 {link}\n\n📛 SESİ AÇ 'a tıklamayı unutma"
-                elif sablon == "1":
+                elif sablon == "2":
                     sablon = f"🔴 {aciklama}\n\n❌ SILINMEDEN IZLE ❌\n\n👉 DEVAMI LİNKTE: {link}\n\n🔔ʙɪʟᴅɪʀɪᴍʟᴇʀɪ ᴀçᴍᴀʏı ᴜɴᴜᴛᴍᴀʏıɴ.\n\n📌 Link Nasıl Açılır Bilmiyorsanız\n\n👉 @linkgecmeth"
                 elif sablon == "3":
                     sablon = f"{aciklama} \n\n         𝙇𝙄𝙉𝙆🔗 {link}\n\n🔔ʙɪʟᴅɪʀɪᴍʟᴇʀɪ ᴀçᴍᴀʏı ᴜɴᴜᴛᴍᴀʏıɴ.\n\n📌 Link Nasıl Açılır Bilmiyorsanız\n\n👉 @linkgec06"
+                else:
+                    soll = sablon.split("{link}")
+                    sal = soll[0].split("{aciklama}")
+                    
+                    sablon = f""
                 sleep(1)
                 bot.send_video(kanal, medya, caption=sablon)
             except Exception as e:
