4d1305d58de5026fba0942ba230ebd22ac71c00e
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-28 11:55:48 +0300

    update main.py

--- main.py
+++ main.py
@@ -203,7 +203,7 @@ def poster(message):
                     sablon = f"🔴 {aciklama}\n\n❌ SILINMEDEN IZLE ❌\n\n👉 DEVAMI LİNKTE: {link}\n\n🔔ʙɪʟᴅɪʀɪᴍʟᴇʀɪ ᴀçᴍᴀʏı ᴜɴᴜᴛᴍᴀʏıɴ.\n\n📌 Link Nasıl Açılır Bilmiyorsanız\n\n👉 @linkgecmeth"
                 print(sablon)
                 sleep(1)
-                bot.send_video(kanal, medya, caption=f"{sablon}")
+                bot.send_video(kanal, medya, caption=f"f"{sablon}"")
             except Exception as e:
                 print(f"Hatalı Kanal: {kanal}")
             print("Başarılı!")
