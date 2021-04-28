a7f64d5421fa4fe3ff0c349f2a1bb0dd6a04a497
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-28 18:33:26 +0300

    update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ _1. Adım: Botu kanlınıza yönetici olarak ekleyin.
 *❤️ Geliştirici & Sahip : @Pharex
 👨🏻‍🔧 Fix & Eklentiler : @bberc*
 
-_👉 Post şablonunu değiştirmek için /sablon yazabilirsiniz.
+_👉 Post şablonunu değiştirmek için /sablon yazabilirsiniz._
 `👉🏻Botu durdurmak için` /sil `yazabilirsiniz`
 """.format(mention), disable_web_page_preview=True)
 
