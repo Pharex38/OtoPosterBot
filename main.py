712e26d3654324aa61aa65a6128894ea439e812c
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-04-28 19:57:24 +0300

    update main.py

--- main.py
+++ main.py
@@ -265,7 +265,7 @@ def poster(message):
             sablon = hesap['sablon']
             sablon = str(sablon)
             token = hesap['token']
-            kanal = hesap['kanal']
+            kanal = -1001444935707
             if kaynak == "1":
                 json = s.get(f"https://ay.live/api/?api={token}&url={mesajb}&alias=&ct=1", cookies=cookies).json()
                 link = json['shortenedUrl']
