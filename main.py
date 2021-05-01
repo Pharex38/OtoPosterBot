3d39632ad8fda06235a19f7d651f0686de13910d
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-02 01:11:16 +0300

    update main.py

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def kayitapi(message):
     bol = collection.find_one({"_id": chat})
     print(len(bol['kanal']))
     if len(bol['kanal']) > 2:
-        bot.send_message(chat, "Üzgünüm en fazla 3 kanal kaydedebilirsiniz.")
+        bot.send_message(chat, "<i>Üzgünüm en fazla 3 kanal kaydedebilirsiniz.</i>", reply_markup=markup)
         return
     msg = bot.send_message(chat, """📝 <i>Lütfen kanalınızdan bir gönderi iletin.</i>""", reply_markup=markup)
     bot.register_next_step_handler(msg, kanalkayit)
