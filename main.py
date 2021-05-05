b70f5716bd678449ebb87b0ed926b746ff2cab9c
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-06 01:10:59 +0300

    update main.py

--- main.py
+++ main.py
@@ -401,6 +401,7 @@ def pat(message):
         plink = pjson['shortenedUrl']
     if psite == "2":
         pjson = s.get(f"https://www.pnd.tl/api?api={ptoken}&url={plink}&category=6").json()
+        print(pjson)
         plink = pjson['shortenedUrl']
     if psite == "3":
         pjson = s.get(f"https://exe.io/api?api={ptoken}&url={plink}").json()
