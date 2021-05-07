3c2219eb0d8c6caa8c64a8047932a83c9eee1f6e
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-07 13:45:31 +0300

    update main.py

--- main.py
+++ main.py
@@ -519,6 +519,12 @@ def pat(message):
         psablon = f"{paciklama} \n\n         𝙇𝙄𝙉𝙆🔗 {plink}\n\n🔔ʙɪʟᴅɪʀɪᴍʟᴇʀɪ ᴀçᴍᴀʏı ᴜɴᴜᴛᴍᴀʏıɴ.\n\n📌 Link Nasıl Açılır Bilmiyorsanız\n\n👉 @linkgec06"
     elif psablon == "9":
         psablon = f"{paciklama} \n\n𝙇𝙄𝙉𝙆🔗 {plink} \n\n     𝙇𝙄𝙉𝙆🔗 {palink}\n\n 🔔ʙɪʟᴅɪʀɪᴍʟᴇʀɪ ᴀçᴍᴀʏı ᴜɴᴜᴛᴍᴀʏıɴ.\n\n 📌 Link Nasıl Açılır Bilmiyorsanız\n👉 @linkk_gecmee"
+    elif sablon.find('{alink}') != -1:
+                    asol = sablon.split("{link}")
+                    aort = asol[1].split("{alink}")
+                    asal = asol[0].split("{aciklama}")
+                    sablon = f"{asal[0]}{aciklama}{asal[1]}{link}{aort[0]}{alink}{aort[1]}"
+                    
                 
     else:
         psoll = psablon.split("{link}")
