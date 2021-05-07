3be9a7249c08f7cd14f885555d49891fc8279692
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-07 11:38:44 +0300

    update main.py

--- main.py
+++ main.py
@@ -236,7 +236,7 @@ def menu(message):
             if bina['altsite'] == None:
                 msg = bot.send_message(chat, "♦️Kayıtlı API: {}\nSite: {}\nToplam Kanal: {}".format(tokenn, site, kayitli), reply_markup=markupp)
             else:
-                msg = bot.send_message(chat, "♦️Kayıtlı API: {}\nSite: {}\nAlternatif Site: {}\nAlternatif API: {}\nToplam Kanal: {}".format(tokenn, site, bina['altsite'], bina['altapi'] kayitli), reply_markup=markupp)
+                msg = bot.send_message(chat, "♦️Kayıtlı API: {}\nSite: {}\nAlternatif Site: {}\nAlternatif API: {}\nToplam Kanal: {}".format(tokenn, site, bina['altsite'], bina['altapi'], kayitli), reply_markup=markupp)
                 
             bot.register_next_step_handler(msg, kayitapi)
             return
