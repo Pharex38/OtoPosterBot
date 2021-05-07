bb13f6d29ceac8bc77e8cb427b5d3599ff72912e
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-07 12:06:46 +0300

    update main.py

--- main.py
+++ main.py
@@ -394,7 +394,7 @@ def altkayit(message):
         bot.send_message(chat, "İptal Edildi.", reply_markup=dugme)
         return
     if message.text == "⛔ Alternatif Kaldır":
-        collection.find_one({"_id": user}, {"$set": {"altsite": None, "altapi": None}})
+        collection.update_one({"_id": user}, {"$set": {"altsite": None, "altapi": None}})
         bot.send_message(chat, "Alternatif kaldırıldı, artık postlarınız alternatif linksiz paylaşılacak.", reply_markup=dugme)
         return
     msg = bot.send_message(chat, "✅ Site kaydedildi!\n\nAlternatif sitenizin API adresinizi gönderin.")
@@ -406,7 +406,7 @@ def altakayit(message, smesaj, user, chat):
         bot.send_message(chat, "İptal Edildi.", reply_markup=dugme)
         return
     if message.text == "⛔ Alternatif Kaldır":
-        collection.find_one({"_id": user}, {"$set": {"altsite": None, "altapi": None}})
+        collection.update_one({"_id": user}, {"$set": {"altsite": None, "altapi": None}})
         bot.send_message(chat, "Alternatif kaldırıldı, artık postlarınız alternatif linksiz paylaşılacak.", reply_markup=dugme)
         return
     collection.update_one({"_id": user}, {"$set": {"altsite": smesaj, "altapi": amesaj, "sablon": "9"}})
