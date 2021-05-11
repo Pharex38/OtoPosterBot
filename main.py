63c5484f53ea4d8f4039fdf97062658797f1f5f3
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-11 03:29:44 +0300

    update main.py

--- main.py
+++ main.py
@@ -106,8 +106,7 @@ def stats(message):
         for kul in kullanici['kanal']:
             kanals += 1
     bot.send_message(chat, "Toplam Kullanıcı Sayısı: {}\nToplam Kayıtlı Kanal Sayısı: {}".format(users, kanals)
-            
-            
+
 
 @bot.message_handler(commands=['sil'])
 def durdur(message):
