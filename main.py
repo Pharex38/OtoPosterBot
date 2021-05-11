7363fc9b36f8cad09e25234c889d6a2d55799314
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-11 22:30:18 +0300

    update main.py

--- main.py
+++ main.py
@@ -1009,7 +1009,7 @@ def gunluk():
                 users += 1
                 for kul in kullanici['kanal']:
                     kanals += 1
-            bot.send_message(chat, "👥 Toplam Kullanıcı Sayısı: {}\n📢 Toplam Kayıtlı Kanal Sayısı: {}\n\nHer gün saat 22:00'da güncel veriler paylaşılacak.".format(users, kanals))
+            bot.send_message(botlog, "👥 Toplam Kullanıcı Sayısı: {}\n📢 Toplam Kayıtlı Kanal Sayısı: {}\n\nHer gün saat 22:00'da güncel veriler paylaşılacak.".format(users, kanals))
         time.sleep(60)
     
 timThr = threading.Thread(target=gunluk)
