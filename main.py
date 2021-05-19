bd5a9b3acc32ee77f6f238233ba53df131821249
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-19 11:33:05 -0700

    update main.py

--- main.py
+++ main.py
@@ -64,10 +64,10 @@ logd = "{}.{}.{} - {}.{}".format(zaman.year, zaman.month, zaman.day, saat, dakik
 
 def setup_logger():
     global logger
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
     file_handler = logging.FileHandler(f'Loglar/{logd}.txt', 'w', 'utf-8')
     stream_handler = logging.StreamHandler()
     logger = logging.getLogger("main_log")
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
 
