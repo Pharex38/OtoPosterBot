33b3d28edb2b5d9319a60c246b94d24815049827
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-21 00:47:00 -0700

    update OtoDowner.py

--- OtoDowner.py
+++ OtoDowner.py
@@ -1,1 +1,15 @@
-***REMOVED-BOT-TOKEN***
+import telebot
+import os
+
+token = "***REMOVED-BOT-TOKEN***"
+bot = telebot.TeleBot(token, parse_mode='html')
+
+@bot.message_handler(commands=['run'])
+def run(m):
+    chat = m.chat.id
+    msg = bot.send_message(chat, "<code>Bot yeniden başlatılıyor</code>")
+    os.system('python main.py')
+    bot.edit_text_message(chat, msg.message_id, "Yeniden Başlatıldı!")
+
+
+bot.polling(none_stop=True)
