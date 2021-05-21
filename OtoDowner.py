dab90cdc9b78a6f099bb6fcd2fc54b7316b8bd5d
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-21 01:23:23 -0700

    update OtoDowner.py

--- OtoDowner.py
+++ OtoDowner.py
@@ -20,7 +20,8 @@ def run(m):
 @bot.message_handler(commands=['stop'])
 def stop(p):
     chat = p.chat.id
-    os.kill(anabot.pid, 9)
+    os.system('pkill -TERM -P {pid}'.format(pid=anabot.pid))
+    #os.kill(anabot.pid, signal.SIGKILL)
     bot.send_message(chat, "Bot Durduruldu.")
 
 
