662528e31682d2794511210dd49f73a1e4eb0b4f
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-25 01:57:25 -0700

    update main.py

--- main.py
+++ main.py
@@ -562,7 +562,7 @@ def gen_markup(user):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == "cb_yes":
-        bot.answer_callback_query(call.id, "Answer is Yes")
+        bot.answer_callback_query(call.id, "Yapım Aşamasında...")
     elif call.data == "cb_no":
         bot.answer_callback_query(
 
