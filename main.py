70866ab952dfc750844e12ad5f5e66c269bbfc90
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-26 14:20:15 -0700

    update main.py

--- main.py
+++ main.py
@@ -944,7 +944,7 @@ def pat(message):
             bot.send_animation(pkanallar[0], fid, caption=psablon)
         bot.send_message(chat, "Postunuz gönderildi.", reply_markup=dugme)
         return
-    if patc.sira == 1:
+    if 1 == patc.sira:
         bot.send_message(chat, "Biraz bekleyin.")
         while patc.sira == 1:
             time.sleep(2)
