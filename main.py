ecccddec38db3ec43641e5e8910b9bb7b7e9f68d
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-25 23:29:10 -0700

    update main.py

--- main.py
+++ main.py
@@ -331,7 +331,7 @@ def menu(message):
         mahzen = bot.get_chat(kaynaklar[0])
         bedava = bot.get_chat(kaynaklar[1])
         evi = bot.get_chat(kaynaklar[2])
-        bashub = bot.get_chat(kaynaklar[3])
+       # bashub = bot.get_chat(kaynaklar[3])
         acikmi = bot.get_chat(kaynaklar[4])
         muho = bot.get_chat(kaynaklar[5])
         tutan = bot.get_chat(kaynaklar[6])
@@ -350,7 +350,7 @@ def menu(message):
     <a href="{}">{}</a>
     
     <b>Kaynak No:4</b>
-    <a href="{}">{}</a>
+    <a href="">gg</a>
     
     <b>Kaynak No:5</b>
     <a href="{}">{}</a>
@@ -365,7 +365,7 @@ def menu(message):
 Örnek: "1,2,3"</b>
     
     
-    """.format(mahzen.invite_link, mahzen.title, bedava.invite_link, bedava.title, evi.invite_link, evi.title, bashub.invite_link, bashub.title, acikmi.invite_link, acikmi.title, muho.invite_link, muho.title, tutan.invite_link, tutan.title), disable_web_page_preview=True, reply_markup=imark)
+    """.format(mahzen.invite_link, mahzen.title, bedava.invite_link, bedava.title, evi.invite_link, evi.title, acikmi.invite_link, acikmi.title, muho.invite_link, muho.title, tutan.invite_link, tutan.title), disable_web_page_preview=True, reply_markup=imark)
         bot.register_next_step_handler(msg, kaynake)
         return
     if mesaj == "📏 Şablon":
