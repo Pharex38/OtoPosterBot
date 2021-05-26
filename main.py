c066e2aacb867a16166dd0ecde05e7428a6d6e76
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-26 10:24:45 -0700

    update main.py

--- main.py
+++ main.py
@@ -453,8 +453,7 @@ def menu(message):
         if mj == None:
             bot.send_message(chat, "Lütfen önce bir API kaydedin.")
             return
-        msg = bot.send_message(chat, """<b>Kullanmak istediğiniz kaynak kanalını seçin.</b>""", reply_markup=kaynakmark(user))
-        bot.register_next_step_handler(msg, kaynake)
+        bot.send_message(chat, """<b>Kullanmak istediğiniz kaynak kanalını seçin.</b>""")
         return
     if mesaj == "📏 Şablon":
         if mj == None:
