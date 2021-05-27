629242089c43ef461809f74f116eb6aed79e16ae
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-05-28 02:58:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -431,7 +431,7 @@ def callback_query(call):
         if dgr == 7:
             bot.answer_callback_query(callback_query_id=call.id,show_alert=True, text=saat['muho'])
     if call.data == "okay":
-        bot.edit_message_text("""<b>Özel Kaynak Hakkında Bilmeniz Gerekenler</b>\n\n<i>\n- Özel kaynak ayarlarsanız başka kaynak seçemezsiniz.\n- Sadece size özeldir başkası kullanamaz.\n- Özel kaynağa kısaltılmamış link atmanız gerekiyor. Kısaltılmış linkli post atarsanız bot linki geçmez direkt olarak kısaltılmış linki tekrar kısaltır.</i>""", chat, mesajid)
+        bot.edit_message_text("""<b>Özel Kaynak Hakkında Bilmeniz Gerekenler</b>\n\n<i>- Özel kaynak ayarlarsanız başka kaynak seçemezsiniz.\n- Sadece size özeldir başkası kullanamaz.\n- Özel kaynağa kısaltılmamış link atmanız gerekiyor. Kısaltılmış linkli post atarsanız bot linki geçmez direkt olarak kısaltılmış linki tekrar kısaltır.</i>\n\n<b>Alttaki butona bastığınız zaman işlem iptal edilemez!</b>""", chat, mesajid)
         bot.edit_message_reply_markup(chat, mesajid, reply_markup=ozelmark())
     if call.data == "okayt":
         msg = bot.edit_message_text("""<b>Yapmanız Gerekenler</b>
