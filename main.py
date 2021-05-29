af1526abd909da7fbcda372c08d961580f9fe225
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-29 14:21:12 -0700

    update main.py

--- main.py
+++ main.py
@@ -252,6 +252,7 @@ def stats(message):
         users += 1
         for kul in kullanici['kanal']:
             if not kul in kum:
+                kum.append(kul)
                 time.sleep(1)
                 kanals += 1
                 try:
@@ -2120,21 +2121,35 @@ def poster(message):
                         logger.debug(f"{okanal} kayıtlardan silindi.")
             logger.info("Başarılı!")
 
-
-
-
 def gunluk():
     while 0 < 1:
         zaman = datetime.datetime.now()
         if zaman.hour == 11 and zaman.minute == 59:
+            msg = bot.send_message(botlog, "<code>Günlük veriler hesaplanıyor...</code>")
+            toplam = 0
+            kum = []
             kanals = 0
             users = 0
             kullanicilar = collection.find({})
             for kullanici in kullanicilar:
                 users += 1
                 for kul in kullanici['kanal']:
-                    kanals += 1
-            msg = bot.send_message(botlog, "👥 Toplam Kullanıcı Sayısı: {}\n📢 Toplam Kayıtlı Kanal Sayısı: {}\n\nHer gün saat 22:00'da otomatik olarak güncel veriler paylaşılacak.".format(users, kanals))
+                    if not kul in kum:
+                        kum.append(kul)
+                        time.sleep(0.5)
+                        try:
+                            uye = bot.get_chat_members_count(kul)
+                            print(uye)
+                        except Exception as e:
+                            logger.error(e)
+                            time.sleep(30)
+                        else:
+                            toplam += uye
+                            kanals += 1
+          
+            toplam = toplam / 1000
+            toplam = round(toplam, 1)
+            msg = bot.edit_message_text("👥 Toplam Kullanıcı Sayısı: {}\n📢 Toplam Kayıtlı Kanal Sayısı: {}\n\nHer gün saat 22:00'da otomatik olarak güncel veriler paylaşılacak.".format(users, kanals), botlog, msg.message_id)
             bot.pin_chat_message(botlog, msg.message_id)
         time.sleep(60)
     
