a68e61611646c49d69115036e8c7e04b1e354743
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-29 13:53:22 -0700

    update main.py

--- main.py
+++ main.py
@@ -261,7 +261,8 @@ def stats(message):
                     logger.error(e)
                     time.sleep(60)
                 toplam += uye
-            
+          
+    toplam = toplam / 1000
     bot.edit_message_text("Toplam Kullanıcı Sayısı: {}\nToplam Kayıtlı Kanal Sayısı: {}\nToplam Kitle: {}".format(users, kanals, toplam), chat, msg.message_id)
 
 @bot.message_handler(commands=['onayla'])
