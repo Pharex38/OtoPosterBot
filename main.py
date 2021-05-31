0a23cef8921d6d8a6aceb1b79c5c2320374b56af
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-05-31 04:32:51 -0700

    update main.py

--- main.py
+++ main.py
@@ -706,21 +706,22 @@ def menu(message):
     if mesaj == "📏 Şablon":
         link = "https://ay.live/vRpKVx"
         aciklama = "Pharex, lord adminin karısını sikerken lord adminn basıyor."
+        alink = "https://pgg.fyi/X0DK3"
         if mj == None:
             bot.send_message(chat, "Lütfen önce bir API kaydedin.")
             return
         if mj['sablon'] == "1":
-            bot.send_message(chat, f"Varsayılan Şablon:\n\n🔥{aciklama} \n\n🔱 TIKLA 👉 {link} \n\n📛 SESİ AÇ 'a tıklamayı unutma", reply_markup=sablonmark(user))
+            bot.send_message(chat, "<b>Varsayılan Şablon:</b>\n\n🔥{aciklama} \n\n🔱 TIKLA 👉 {link} \n\n📛 SESİ AÇ 'a tıklamayı unutma", reply_markup=sablonmark(user))
         elif mj['sablon'] == "2":
-            bot.send_message(chat, f"Varsayılan Şablon:\n\n{aciklama} \n\n         𝙇𝙄𝙉𝙆🔗 {link}\n\n🔔ʙɪʟᴅɪʀɪᴍʟᴇʀɪ ᴀçᴍᴀʏı ᴜɴᴜᴛᴍᴀʏıɴ.\n\n📌 Link Nasıl Açılır Bilmiyorsanız\n\n👉 @linkgec06", reply_markup=sablonmark(user))
+            bot.send_message(chat, "<b>Varsayılan Şablon:</b>\n\n{aciklama} \n\n         𝙇𝙄𝙉𝙆🔗 {link}\n\n🔔ʙɪʟᴅɪʀɪᴍʟᴇʀɪ ᴀçᴍᴀʏı ᴜɴᴜᴛᴍᴀʏıɴ.\n\n📌 Link Nasıl Açılır Bilmiyorsanız\n\n👉 @linkgec06", reply_markup=sablonmark(user))
         elif mj['sablon'] == "9":
-            bot.send_message(chat, f"Varsayılan Şablon:\n\n{aciklama} \n\n𝙇𝙄𝙉𝙆🔗 {link} \n\n     𝙇𝙄𝙉𝙆🔗 {alink}\n\n 🔔ʙɪʟᴅɪʀɪᴍʟᴇʀɪ ᴀçᴍᴀʏı ᴜɴᴜᴛᴍᴀʏıɴ.\n\n 📌 Link Nasıl Açılır Bilmiyorsanız\n👉 @linkk_gecmee", reply_markup=sablonmark(user))
+            bot.send_message(chat, "<b>Varsayılan Şablon:</b>\n\n{aciklama} \n\n𝙇𝙄𝙉𝙆🔗 {link} \n\n     𝙇𝙄𝙉𝙆🔗 {alink}\n\n 🔔ʙɪʟᴅɪʀɪᴍʟᴇʀɪ ᴀçᴍᴀʏı ᴜɴᴜᴛᴍᴀʏıɴ.\n\n 📌 Link Nasıl Açılır Bilmiyorsanız\n👉 @linkk_gecmee", reply_markup=sablonmark(user))
         else:
             if mj['sira'] == "1":
                 pst = mj['sablon'].replace("{aciklama}", "{}").replace("{link}", "{}").replace("{alink}", "{}").format(aciklama, link, alink)
             else:
                 pst = mj['sablon'].replace("{aciklama}", "{}").replace("{link}", "{}").format(aciklama, link)
-            bot.send_message(chat, f"Şablonunuz böyle gözükecek:\n\n{pst}", reply_markup=sablonmark(user))
+            bot.send_message(chat, f"<b>Şablonunuz böyle gözükecek:</b>\n\n{pst}", reply_markup=sablonmark(user))
         return
     if mesaj == "📝 Kaydet":
         try:
