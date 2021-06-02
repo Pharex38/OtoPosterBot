172c4b76780d5727a2b84e1d51b29a59123cac8e
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-06-02 12:33:40 +0300

    Update OtoDowner.py

--- OtoDowner.py
+++ OtoDowner.py
@@ -26,7 +26,7 @@ def is_running():
                 simdisaat = nzaman.hour - bzaman.hour
                 simdidakika = nzaman.minute - bzaman.minute
                 simdisaniye = nzaman.second - bzaman.second
-                sure = str(simdisaat)+":"+str(simdidakika)+":"+str(simdisaniye)
+                sure = str(simdisaat.zfill(2))+":"+str(simdidakika.zfill(2))+":"+str(simdisaniye.zfill(2))
                 logs.info(f"{sure} İşlem hâlâ çalışıyor.")
                 return True, sure
     logs.warning("İşlem Bulunamadı.")
@@ -58,9 +58,9 @@ logs.info("Eski İşlem Kapatıldı")
 def send_welcome(message):
     chat = message.chat.id
     pid = open("pid.txt", "r").read()
-    if is_running():
+    yes, sure = is_running()
+    if yes:
         durum = "Aktif!"
-        sure = is_running()
         bot.send_message(chat, "Merhaba!\n\nDurum: {}\nÇalışma Süresi: <code>{}</code>".format(durum, sure)) 
     else:
         durum = "Kapalı!"
