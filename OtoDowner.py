5460046c44c4ddb2f77e07450a28fb0537664e3b
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-06-03 06:02:44 -0700

    update OtoDowner.py

--- OtoDowner.py
+++ OtoDowner.py
@@ -22,7 +22,7 @@ def is_running():
     for q in psutil.process_iter():
         if q.name().startswith('python'):
             if q.pid == int(anapid):
-                print(type(q.started))
+                print(q)
                 suan = time.time()
                 # Biraz Matematik
                 saniye = int(suan - baslangic)
