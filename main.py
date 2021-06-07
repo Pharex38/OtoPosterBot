9490ae08cde89c5da9109c45f01631246ee1ab74
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-06-07 11:54:29 -0700

    update main.py

--- main.py
+++ main.py
@@ -2382,7 +2382,7 @@ def poster(message):
 def gunluk():
     while 0 < 1:
         zaman = datetime.datetime.now()
-        if zaman.hour == 11 and zaman.minute == 50:
+        if zaman.hour == 11 and zaman.minute == 56:
             msg = bot.send_message(botlog, "<code>Günlük veriler hesaplanıyor...</code>")
             toplam = 0
             kum = []
