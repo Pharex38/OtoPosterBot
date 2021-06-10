65995212cef4c64bbaa2503bfc6cdfeb2dba3968
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-06-10 13:04:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2364,7 +2364,7 @@ logger.info("Bot Çalışıyor...")
 bildir('Bot Başladı 🍕')
 
 def main() -> None:
-    updater = Updater(bot=bot, request_kwargs={'read_timeout': 6, 'connect_timeout': 7})
+    updater = Updater(bot=bot, request_kwargs={'read_timeout': 8, 'connect_timeout': 9})
 
     dispatcher = updater.dispatcher
     conv_handler = ConversationHandler(
