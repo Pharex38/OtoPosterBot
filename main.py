d0d120245df6d962084c91e4ae79f81ea9a9834a
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-06-10 09:40:09 -0700

    update main.py

--- main.py
+++ main.py
@@ -2374,7 +2374,7 @@ def main() -> None:
 
     dispatcher = updater.dispatcher
     conv_handler = ConversationHandler(
-        entry_points=[MessageHandler(Filters.update.message & ~Filters.command, menu)],
+        entry_points=[MessageHandler(Filters.update.message & Filters.chat_type.private & ~Filters.command, menu)],
         states={ 
             ALTMENU: [MessageHandler(Filters.text & Filters.update.message, kayitapi)], 
             APIDEGISTIR: [MessageHandler(Filters.text & Filters.update.message, apikayit)],
