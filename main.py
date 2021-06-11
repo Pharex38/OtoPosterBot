200f79e824d7bce016ce5e442204d5de512aac3d
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-06-11 14:14:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ fixer = 1687646994
 adminlist = [1687646994,1302980840]
 
 for p in OzelCol.find({}):
-    OzelCol.update_one({"_id": p['_id']}, {"$set"{"log": "yok"}})
+    OzelCol.update_one({"_id": p['_id']}, {"$set": {"log": "yok"}})
 
 def bildir(neyi='Boş Bildirim Testi !'):
     for i in adminlist:
