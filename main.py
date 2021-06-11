8c77200a9d20c45a8557a53348e1cf8043f99bc2
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-06-11 12:22:06 -0700

    update main.py

--- main.py
+++ main.py
@@ -2406,7 +2406,6 @@ def gunluk():
                         time.sleep(0.5)
                         try:
                             uye = bot.get_chat_members_count(kul)
-                            print(uye)
                         except Exception as e:
                             logger.error(e)
                             time.sleep(30)
@@ -2417,7 +2416,7 @@ def gunluk():
             toplam = toplam / 1000
             toplam = str(round(toplam, 1))+"K" if round(toplam, 1) < 1000 else str(round(toplam / 1000, 1))+"M"
             msg = bot.edit_message_text("👥 Toplam Kullanıcı Sayısı: {}\n📢 Toplam Kayıtlı Kanal Sayısı: {}\n🙋 Toplam Kitle: {}\n\nHer gün saat 22:00'da otomatik olarak güncel veriler paylaşılacak.".format(users, kanals, toplam), botlog, msg.message_id)
-            update.message.pin_chat_message(botlog, msg.message_id)
+            bot.pin_chat_message(botlog, msg.message_id)
         time.sleep(60)
     
 threading.Thread(target=gunluk).start()
