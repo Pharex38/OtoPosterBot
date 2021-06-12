c849ac0124fc34cdb4b5b7bb3dcb768ff6a5e85e
berce <75228263+must4f@users.noreply.github.com>
2021-06-12 13:00:22 +0300

    Update __init__.py

--- otoposter/__init__.py
+++ otoposter/__init__.py
@@ -1,6 +1,7 @@
 import os
+from telegram import ParseMode
 from pymongo import MongoClient
-from telegram.ext import ExtBot
+from telegram.ext import ExtBot, Defaults
 
 mpass = os.environ['MONGOPASS']
 mongo = f"os.environ["MONGO_URI"]"
@@ -25,3 +26,16 @@ botlog = -1001352123979
 sahip = 1302980840
 fixer = 1687646994
 adminlist = [1687646994,1302980840]
+
+def setup_logger():
+    global logger
+    file_handler = logging.FileHandler(f'Loglar/{logd}.txt', 'w', 'utf-8')
+    stream_handler = logging.StreamHandler()
+    logger = logging.getLogger("main_log")
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+#logger = getLogger(__name__)
+
+setup_logger()
