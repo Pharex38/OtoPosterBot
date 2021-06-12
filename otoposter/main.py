f3c44112625ed66af4a2f5bae2d2ac34dde79b93
berce <75228263+must4f@users.noreply.github.com>
2021-06-12 14:00:42 +0300

    Update main.py

--- otoposter/main.py
+++ otoposter/main.py
@@ -27,14 +27,13 @@ def error_handler(update: object, context: CallbackContext) -> None:
     for i in adminlist:
         context.bot.send_message(chat_id=i, text=message, parse_mode=ParseMode.HTML)
 
+updater = Updater(token=bottoken, defaults=Defaults(parse_mode=ParseMode.HTML, run_async=True, timeout=90), request_kwargs={'con_pool_size': 999, 'read_timeout': 150, 'connect_timeout': 150})
+
+dispatcher = updater.dispatcher
+
+updater.job_queue
 
 def main() -> None:
-    updater = Updater(token=bottoken, defaults=Defaults(parse_mode=ParseMode.HTML, run_async=True, timeout=90), request_kwargs={'con_pool_size': 999, 'read_timeout': 150, 'connect_timeout': 150})
-
-    dispatcher = updater.dispatcher
-
-    updater.job_queue
-
     dispatcher.add_error_handler(error_handler)
 
     updater.start_polling()
