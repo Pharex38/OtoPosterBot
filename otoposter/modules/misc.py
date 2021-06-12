85e83b5c65181ae18b40df98a862f45f49de0518
berce <75228263+must4f@users.noreply.github.com>
2021-06-12 15:08:03 +0300

    Update misc.py

--- otoposter/modules/misc.py
+++ otoposter/modules/misc.py
@@ -1,5 +1,6 @@
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton, ForceReply, InlineKeyboardMarkup, InlineKeyboardButton
 from otoposter import *
+from .markup import *
 
 kaynaklar = [-1001368112299, -1001122395785, -1001423365614, -1001240514861, -1001405966343, -1001368008488, -1001379893661]
 
