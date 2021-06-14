04f81299805f69e5b14bb389a423e74859a596f9
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-06-14 11:52:31 -0700

    update main.py

--- main.py
+++ main.py
@@ -2577,7 +2577,7 @@ def poster(update, context):
 def gunluk():
     while 0 < 1:
         zaman = datetime.datetime.now()
-        if zaman.hour == 11 and zaman.minute == 50:
+        if zaman.hour == 11 and zaman.minute == 55:
             ozel_kaynak_kullanan_sayisi, mahzen_kullanan_sayisi, hazır_kullanan_sayisi, tutan_kullanan_sayisi, acikmi_kullanan_sayisi, bedava_kullanan_sayisi, evi_kullanan_sayisi, bashub_kullanan_sayisi = 0, 0, 0, 0, 0, 0, 0, 0
             exe_kullanan_sayisi, pubiza_kullanan_sayisi, ouo_kullanan_sayisi, trlink_kullanan_sayisi, pnd_kullanan_sayisi = 0, 0, 0, 0, 0
             msg = bot.send_message(botlog, "<code>Günlük veriler hesaplanıyor...</code>")
