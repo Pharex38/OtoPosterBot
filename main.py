a0d7beb7e3f6632350075bf82b14ffdf8136487f
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-06-14 13:07:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ def dugme(user):
     return dugme
 
 def markupp():
-    markupp = ReplyKeyboardMarkup(keyboard=[['🔶 Yeni Kanal Ekle', '🗑️ Kanal Sil'], ['♻️ API değiştir', '🔗 Site değiştir'], ['🤖 Alternatif Ekle'], ['↩️ Ana Menü']], row_width=2, one_time_keyboard=True, resize_keyboard=True)
+    markupp = ReplyKeyboardMarkup(keyboard=[['🔶 Yeni Kanal Ekle', '🗑️ Kanal Sil'], ['♻️ API değiştir', '🔗 Site değiştir'], ['🤖 Alternatif Ekle'], ['↩️ Ana Menü']], row_width=2, one_time_keyboard=False, resize_keyboard=True)
 
     return markupp
 
