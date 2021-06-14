325832fac9c336733b12ccec3ed2dd3840664215
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-06-15 02:34:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -332,7 +332,7 @@ def durdur(update, context):
     kimi = int(update.message.text.split()[1]) if len(update.message.text.split()) > 1 and user in adminlist else update.message.from_user.id
     if collection.find_one({"_id": kimi})['ozel']:
         for oc in OzelCol.find({}):
-            if kimi in oc['kanal']
+            if kimi in oc['kanal']:
                 OzelCol.update_one({"_id": oc['_id']}, {"$pull": {"kanal": kimi}})
                 break
     collection.delete_one({"_id": kimi})
