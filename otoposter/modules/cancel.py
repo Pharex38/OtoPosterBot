91d92e9cc788b4e5a116cae7016901458bf34350
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-06-21 13:10:48 +0300

    gg

--- otoposter/modules/cancel.py
+++
@@ -1,7 +0,0 @@
-from otoposter import *
-from .misc import dugme
-
-def cancel(update, context):
-    chat = update.message.chat.id
-    bot.send_message(chat, "İptal Edildi.", reply_markup=dugme(chat))
-    return
