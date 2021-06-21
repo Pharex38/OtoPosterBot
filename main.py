e518d8abd12f256b14d67b5b51cbc84cc413fad3
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-06-21 15:11:39 -0700

    update main.py

--- main.py
+++ main.py
@@ -2568,6 +2568,10 @@ def poster(update, context):
     else:
         okaynak = OzelCol.find_one({"okaynak": chat})
     if okaynak != None:
+        try:
+            print(update.message.media_group_id)
+        except:
+            pass
         ocount = 0
         omesaj = update.channel_post.caption
         if omesaj == None:
