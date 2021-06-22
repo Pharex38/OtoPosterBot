4aeaae7edf5bd5656d7a38e09c3b03e563ef6929
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-06-22 17:16:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1392,14 +1392,11 @@ def patzamansaat(update, context):
         return
     if dak < 0:
         dak = dak + 60
-        satt = True
+        sat += 1
     print(str(sat)+" - "+str(dak))
     if sat <= 0:
         sat = sat + 24
         day += 1
-    print(str(sat)+" - "+str(dak))
-    if satt:
-        sat += 1
     print(str(sat)+" - "+str(dak))
     print(int(sat) * 3600 + int(dak) * 60 + int(day) * 86400)
     context.user_data['zaman'] = int(sat) * 3600 + int(dak) * 60 + int(day) * 86400
