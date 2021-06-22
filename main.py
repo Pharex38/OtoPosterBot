76078700c925712b76418da555c691ad11b2425a
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-06-22 10:14:14 -0700

    update main.py

--- main.py
+++ main.py
@@ -947,7 +947,7 @@ def jobmark(user, context):
         if jop.name.startswith(str(user)):
             jobstr = str(jop.job)
             jnam = jobstr.find("date[")
-            jname = jobstr[jnam+5:-3]
+            jname = jobstr[jnam+5:19:]
             ujcount += 1
             jobkeyb.append([InlineKeyboardButton(jname, callback_data="jop-{}".format(jcount))])
         jcount += 1
