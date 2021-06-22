e7417ea2134937e4374731d8831fabdd5f4d9a59
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-06-22 13:44:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -680,6 +680,9 @@ def callback_query(call, context):
         return PATZAMAN
     if call.callback_query.data == "simdi": 
         bot.delete_message(user, mesajid)
+        ptip = context.user_data['ptip']
+        fid = context.user_data['fid']
+        psablon = context.user_data['psablon']
         if len(collection.find_one({"_id": user})['kanal']) < 2:
             SEND_MEDIA_TYPES[ptip](collection.find_one({"_id": user})['kanal'][0], fid, caption=psablon)
             bot.send_message(user, "Postunuz gönderildi.", reply_markup=dugme(user))
