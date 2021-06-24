c2b7cb6f668e7d56196b54aa966d73d2f0ffeaf0
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-06-24 17:14:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1479,7 +1479,7 @@ def pat(update, context):
     """Link Tespit"""
     psol = mesaj.find("http")
     psag = mesaj.find("\n", psol)
-    print(update.message.parse_entities())
+    print(update.message.parse_caption_entities())
     plink = mesaj[psol:psag].strip()
     if plink.startswith("https://ay") or plink.startswith("https://pgg") or plink.startswith("https://pnd") or plink.startswith("https://ouo") or plink.startswith("https://exe") or plink.startswith("https://lnk"):
         bot.send_message(chat, "Oops sanırım zaten kısaltılmış bir linki kısaltmaya çalışıyorsun. Üzgünüm bu bot linkleri kendisi geçemez.", reply_markup=imark())
