9d237c23c7b41d817eff76bafa22fdad16290391
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-06-25 02:24:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -466,7 +466,7 @@ def duy(update, context):
         duyurumsg = update.message.reply_to_message.text
         kullanicilar = collection.find({})
         for kullanici in kullanicilar:
-            if len(kullanici['kanal']) > 0
+            if len(kullanici['kanal']) > 0:
                 try:
                     dmsg = bot.send_message(kullanici['_id'], duyurumsg, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("😕 Bilgilerimi sil", callback_data="dsil")], [InlineKeyboardButton("✅ Kullanmaya devam etmek istiyorum.", callback_data="devam")]]))
                 except Exception as e:
