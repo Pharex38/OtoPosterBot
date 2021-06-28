74b9d59c0acb03e400878f8529cab01688700a4b
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-06-29 02:07:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def bildir(neyi='Boş Bildirim Testi !'):
         except:
             pass
 
-kaynaklar = [-1001368112299, -1001122395785, -1001423365614, -1001240514861, -1001405966343, -1001368008488, -1001379893661, -1001572457634]
+kaynaklar = [-1001368112299, -1001122395785, -1001423365614, -1001240514861, -1001405966343, -1001368008488, -1001379893661]
 
 SEND_MEDIA_TYPES = {"document": bot.send_document, "photo": bot.send_photo, "video": bot.send_video, "animation": bot.send_animation}
 
