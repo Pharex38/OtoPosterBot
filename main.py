bcb20f1d1ef2fc12b88fb597e9087bf62cead6d6
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-07-02 14:31:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1873,7 +1873,7 @@ def poster(update, context):
                                     opost = bot.send_animation(okan, omedya, caption=osablon)
                             else:
                                 poster_dict = {"update": update, "okan": okan, "omedya": omedya, "osablon": osablon}
-                                job.run_once(zamanalamajob name=str(ouser), context=, when=2, )
+                                job.run_once(zamanalamajob name=str(ouser), context=poster_dict, when=2, )
                     except Exception as e:
                         if str(e).find("Chat is not found") != -1 or str(e).find("Need administrator") != -1 or str(e).find("bot is not") != -1:
                             try:
@@ -1898,15 +1898,17 @@ def poster(update, context):
 
 async def zamanalamajob(context):
     contz = context.job.context
+    update = contz['update']
     async with TelegramClient("eklenti", api_id, api_hash) as app:
-        odmedya = app.download_media(omedya)
-        app.send_message(sahip, "sss")
+        odmedya = await app.download_media(omedya)
+        await app.send_message(sahip, "sss")
         if update.channel_post.photo:
-            app.send_photo(chat_id=okan, photo=odmedya, caption=osablon, schedule=otarih)
+            await app.send_photo(chat_id=contz['okan'], photo=contz['odmedya'], caption=contz['osablon'], schedule=contz['otarih'])
         if update.channel_post.video:
-            app.send_video(chat_id=okan, video=odmedya, caption=osablon, schedule=otarih)
+            await app.send_video(chat_id=contz['okan'], video=contz['odmedya'], caption=contz['osablon'], schedule=contz['otarih'])
         if update.channel_post.animation:
-            app.send_animation(chat_id=okan, animation=odmedya, caption=osablon, schedule=otarih)
+            await app.send_animation(chat_id=contz['okan'], animation=contz['odmedya'], caption=contz['osablon'], schedule=contz['otarih'])
+        os.remove(odmedya)
 
 def gunluk(context):
     ozel_kaynak_kullanan_sayisi = 0
