b1ad42de0d09c8e5a3298bf32c57961120e2e657
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-07-02 03:29:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1756,7 +1756,7 @@ def poster(update, context):
             ovakitler = [ot for ot in ohesap['vakit']] if ohesap['vakit'] != 0 else 0
             odailycount = ohesap['time']
             if ovakitler != 0:
-                otarih = datetime.datetime.strptime(ovakitler[odailycount], "%H:%M")timestamp()
+                otarih = datetime.datetime.strptime(ovakitler[odailycount], "%H:%M").timestamp()
             collection.update_one({"_id": ouser}, {"$inc": {"time": 1}})
             if len(okanal) > 0 and oret:
                 oalink = " "
