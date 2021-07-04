e24322d15ffcf16e7308678afb2ac233d5fd6ffc
berce <75228263+must4f@users.noreply.github.com>
2021-07-04 18:54:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ api_hash = "***REMOVED-API-HASH***"
 appstr = maindata['string']
 
 bot = ExtBot(bottoken, defaults=Defaults(parse_mode=ParseMode.HTML, run_async=True, timeout=99))
-app = Client("appis", api_id, api_hash)
+app = Client(appstr, api_id, api_hash)
 app.start()
 
 blog = -1001391561285
