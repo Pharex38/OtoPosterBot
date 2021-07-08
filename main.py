628a12ce0bccaadcfd34f39f87ea4548531ed4cd
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-07-08 17:53:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ def setup_logger():
     global logger
     zaman = datetime.datetime.now()
     logd = "{}.{}.{} - {}.{}".format(zaman.year, zaman.month, zaman.day, zaman.hour, zaman.minute)
-    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", handlers=[logging.FileHandler(f'Loglar/{logd}.txt', 'w', 'utf-8'), logging.StreamHandler()], level=logging.DEBUG)
+    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", handlers=[logging.FileHandler(f'Loglar/{logd}.txt', 'w', 'utf-8'), logging.StreamHandler()], level=logging.WARNING)
     logger = logging.getLogger("OtoPosterBot")
 
 
