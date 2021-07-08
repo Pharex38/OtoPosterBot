f949ce86dc93e044569b3dd929d82047c27bb68f
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-07-08 10:55:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -278,6 +278,7 @@ def stats(update, context):
 def joblist(update, context):
      jobs = context.job_queue.jobs()
      context.job_queue.run_once(jobyedekleme, when=1, name="yedekleme")
+     context.job_queue.run_once(resetleme, when=2, name="yedekleme")
      for jok in jobs:
         if str(jok.name) != "yedekleme" or str(jok.name) != "gunluk":
             bot.send_message(update.message.chat.id, str(jok.context)+"\n\n\n"+str(jok.name)+"\n\n\n"+str(jok.job))
