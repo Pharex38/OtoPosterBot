86655e3e11451a5577ed5ddb36e942a1df20e682
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-07-09 17:08:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -531,20 +531,25 @@ def error_handler(update: object, context: CallbackContext) -> None:
     tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
     tb_string = ''.join(tb_list)
     update_str = update.to_dict() if isinstance(update, Update) else str(update)
-    if update.message.chat.id in postsirasi:
-        for er in postsirasi:
-            if update.message.chat.id == er['chatid']:
-                try:
-                    postsirasi.remove(er)
-                except Exception as e:
-                    logger.error(e)
-    if update.message.chat.id in opostsirasi
-        for oer in opostsirasi:
-            if update.message.chat.id == oer['chatid']:
-                try:
-                    postsirasi.remove(oer)
-                except Exception as e:
-                    logger.error(e)
+    try:
+        update.message.chat.id
+    except:
+        pass
+    else:
+        if update.message.chat.id in postsirasi:
+            for er in postsirasi:
+                if update.message.chat.id == er['chatid']:
+                    try:
+                        postsirasi.remove(er)
+                    except Exception as e:
+                        logger.error(e)
+        elif update.message.chat.id in opostsirasi:
+            for oer in opostsirasi:
+                if update.message.chat.id == oer['chatid']:
+                    try:
+                        postsirasi.remove(oer)
+                    except Exception as e:
+                        logger.error(e)
     message = (
         f'BİR HATA OLUŞTU!\n'
         f'<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}'
