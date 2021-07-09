0dc1a64311726b2b39436d833745c06341058c3c
Pharex38 <alperenfahrikocakaplan@gmail.com>
2021-07-09 15:40:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2171,6 +2171,7 @@ def eklentiiletisim(update, context):
         return
 
 def comment(update, context):
+    print(update)
     if update.message.text.find("kanalda post paylaşıldı.") == -1 or update.message.text.find("paylaşılıyor") == -1:
         return
     bot.delete_message(update.message.chat.id, update.effective_message.message_id)
