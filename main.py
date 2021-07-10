a56be800d72c88a5ff77b200fcbbfee3a10a818c
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-07-10 16:51:33 +0300

    update main.py

--- main.py
+++ main.py
@@ -382,6 +382,8 @@ def kpostsil(update, context):
         return
     data = db[str(chat)].find({"mesih": mesid})
     spcount = 0
+    for kpsd in collection.find({}):
+        collection.update_one({"_id": kpsd['_id']}, {"$set": {"pcount": kpsd['pcount']-1}})
     for d in data:
         try:
             bot.delete_message(d['chat'], d['pid'])
