4f177acff126edb570c79859f5e60902e55370a7
Pharex <alperenfahrikocakaplan@gmail.com>
2021-07-19 01:18:21 +0300

    Update komutlar.py

--- OtoPoster/komutlar.py
+++ OtoPoster/komutlar.py
@@ -298,12 +298,16 @@ def posterkomut2(update, context):
     if KaynakCol.find_one({"_id": pochat}) != None:
         logger.warning(f"{update.channel_post.chat.title} Postu sıraya eklendi.")
         postdict = {"chatid": pochat, "update": update}
-        postsirasi.append(postdict)
+        while len(context.job_queue.get_jobs_by_name("anaposter")) > 1:
+            bildir(len(context.job_queue.get_jobs_by_name("anaposter")))
+        context.job_queue.run_once(poster_job, when=2, name="anaposter", context=postdict)
     # Özel Kaynaklar
     elif OzelCol.find_one({"okaynak": pochat}) != None:
         logger.warning(f"[ÖZEL] {update.channel_post.chat.title} Postu sıraya eklendi.")
         opostdict = {"chatid": pochat, "update": update}
-        opostsirasi.append(opostdict)
+        while len(context.job_queue.get_jobs_by_name("ozelposter")) > 1:
+            sleep(1)
+        context.job_queue.run_once(ozel_poster_job, when=2, name="ozelposter", context=opostdict)
 
 
 def duy(update, context):
