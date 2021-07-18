d02c6eecb99401ab3cf887a752b0d44fd77e33d0
Pharex <alperenfahrikocakaplan@gmail.com>
2021-07-19 01:11:50 +0300

    Update poster.py

--- OtoPoster/poster.py
+++ OtoPoster/poster.py
@@ -564,14 +564,14 @@ def poster(update, context):
     if KaynakCol.find_one({"_id": pochat}) != None:
         logger.warning(f"{update.channel_post.chat.title} Postu sıraya eklendi.")
         postdict = {"chatid": pochat, "update": update}
-        if len(context.job_queue.get_jobs_by_name("anaposter")) < 2:
+        while len(context.job_queue.get_jobs_by_name("anaposter")) > 1:
             bildir(len(context.job_queue.get_jobs_by_name("anaposter")))
-            context.job_queue.run_once(poster_job, when=2, name="anaposter", context=postdict)
+        context.job_queue.run_once(poster_job, when=2, name="anaposter", context=postdict)
     # Özel Kaynaklar
     elif OzelCol.find_one({"okaynak": pochat}) != None:
         logger.warning(f"[ÖZEL] {update.channel_post.chat.title} Postu sıraya eklendi.")
         opostdict = {"chatid": pochat, "update": update}
-        if len(context.job_queue.get_jobs_by_name("ozelposter")) < 2:
-            bildir(len(context.job_queue.get_jobs_by_name("ozelposter")))
-            context.job_queue.run_once(ozel_poster_job, when=2, name="ozelposter", context=opostdict)
+        while len(context.job_queue.get_jobs_by_name("ozelposter")) > 1:
+            sleep(1)
+        context.job_queue.run_once(ozel_poster_job, when=2, name="ozelposter", context=opostdict)
 
