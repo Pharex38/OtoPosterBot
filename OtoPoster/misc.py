030f2f9fd79c1ea738090b4e93532ae49385ac05
Pharex <alperenfahrikocakaplan@gmail.com>
2021-07-18 12:11:33 +0300

    Update misc.py

--- OtoPoster/misc.py
+++ OtoPoster/misc.py
@@ -123,12 +123,12 @@ def comment(update, context):
     bot.delete_message(update.message.chat.id, update.effective_message.message_id)
 
 def error_handler(update: object, context: CallbackContext) -> None:
-    global postsirasi, opostsirasi
-    print(msg="Bir Hata oluştu:", exc_info=context.error)
-    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
-    tb_string = ''.join(tb_list)
-    update_str = update.to_dict() if isinstance(update, Update) else str(update)
     try:
+        global postsirasi, opostsirasi
+        print(msg="Bir Hata oluştu:", exc_info=context.error)
+        tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+        tb_string = ''.join(tb_list)
+        update_str = update.to_dict() if isinstance(update, Update) else str(update)
         try:
             update.message.chat.id
         except:
@@ -148,17 +148,18 @@ def error_handler(update: object, context: CallbackContext) -> None:
                             postsirasi.remove(oer)
                         except Exception as e:
                             print(e)
-    except Exception as es:
-        print(es)
-    message = (
+        message = (
         f'BİR HATA OLUŞTU!\n'
         f'<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}'
         '</pre>\n\n'
         f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
         f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
         f'<pre>{html.escape(tb_string)}</pre>'
-    )
+        )
 
-    context.bot.send_message(chat_id=1302980840, text=message, parse_mode=ParseMode.HTML)
+        context.bot.send_message(chat_id=1302980840, text=message, parse_mode=ParseMode.HTML)
+    except Exception as es:
+        print(es)
+
 
 setup_logger()
