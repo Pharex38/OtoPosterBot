5359991464d49644c92ec07750e864bee1678f18
Pharex <alperenfahrikocakaplan@gmail.com>
2021-07-19 00:42:46 +0300

    Update poster.py

--- OtoPoster/poster.py
+++ OtoPoster/poster.py
@@ -565,11 +565,13 @@ def poster(update, context):
         logger.warning(f"{update.channel_post.chat.title} Postu sıraya eklendi.")
         postdict = {"chatid": pochat, "update": update}
         if len(context.job_queue.get_jobs_by_name("anaposter")) < 2:
+            bildir(len(context.job_queue.get_jobs_by_name("anaposter")))
             context.job_queue.run_once(poster_job, when=2, name="anaposter", context=postdict)
     # Özel Kaynaklar
     elif OzelCol.find_one({"okaynak": pochat}) != None:
         logger.warning(f"[ÖZEL] {update.channel_post.chat.title} Postu sıraya eklendi.")
         opostdict = {"chatid": pochat, "update": update}
         if len(context.job_queue.get_jobs_by_name("ozelposter")) < 2:
+            bildir(len(context.job_queue.get_jobs_by_name("ozelposter")))
             context.job_queue.run_once(ozel_poster_job, when=2, name="ozelposter", context=opostdict)
 
