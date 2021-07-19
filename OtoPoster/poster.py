86a2dd11fdacd7aa5d0242b9b53ff4de7b0d349b
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-07-19 14:57:13 +0300

    update poster.py

--- OtoPoster/poster.py
+++ OtoPoster/poster.py
@@ -566,6 +566,7 @@ def postsiralandirici(context):
     joblananpostlar = context.job_queue.get_jobs_by_name("anaposter")
     while len(joblananpostlar) > 1:
         joblananpostlar = context.job_queue.get_jobs_by_name("anaposter")
+        bildir(len(joblananpostlar))
         sleep(1)
     for postes in postsirasi:
         context.job_queue.run_once(poster_job, when=2, name="anaposter", context=postes)
