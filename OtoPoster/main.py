91690aaef52e55e0b6eba891b59ed7518f35894f
Pharex <alperenfahrikocakaplan@gmail.com>
2021-07-28 14:06:43 +0300

    Update main.py

--- OtoPoster/main.py
+++ OtoPoster/main.py
@@ -55,6 +55,7 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler('vip', viple, Filters.chat(sahip)))
     dispatcher.add_handler(CommandHandler('apiban', apibanla, Filters.chat(sahip)))
     dispatcher.add_handler(CommandHandler('joblist', joblist, Filters.chat(sahip)))
+    dispatcher.add_handler(CommandHandler('postsil', cpostsil, Filters.chat(sahip)))
     dispatcher.add_handler(CommandHandler('unban', unbanla, Filters.chat(sahip)))
     dispatcher.add_handler(CommandHandler('para', parak, Filters.chat(sahip)))
     dispatcher.add_handler(CommandHandler('poster', posterkomut, Filters.chat(sahip)))
