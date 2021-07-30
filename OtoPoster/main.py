a95fa529a62495767aa7da0f962852a5bf432e76
Pharex <alperenfahrikocakaplan@gmail.com>
2021-07-30 13:02:32 +0300

    Update main.py

--- OtoPoster/main.py
+++ OtoPoster/main.py
@@ -110,6 +110,7 @@ def main() -> None:
     """ Poster """
     dispatcher.add_handler(MessageHandler(Filters.photo & Filters.update.channel_post | Filters.video & Filters.update.channel_post | Filters.animation & Filters.update.channel_post, poster, run_async=False))
     """ Callbacks """
+    dispatcher.add_handler(CallbackQueryHandler(cekiliscall, pattern="^katil(.*)"))
     dispatcher.add_handler(CallbackQueryHandler(kaynakcall, pattern="^kaynak(.*)"))
     dispatcher.add_handler(CallbackQueryHandler(callback_query))
     """ Error Handler """
