b46e6977f9ef662a4347bd654628e32a45f1cfef
Pharex <alperenfahrikocakaplan@gmail.com>
2021-08-03 10:54:53 +0300

    OtoPoster/anafonks:

--- OtoPoster/anafonks.py
+++ OtoPoster/anafonks.py
@@ -14,7 +14,6 @@ def menu(update, context):
     if user in kara:
         bot.send_message(chat, "🤓 Üzgünüm senin gibi aptal birisi için çalışmıyorum")
         return
-    
     try:
         tokenn = mj['token']
     except:
