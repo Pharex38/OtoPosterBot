fd38ae7f46922299a1b74750f29a8a88fcd9a862
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-08-06 08:00:13 +0300

    update poster.py

--- OtoPoster/poster.py
+++ OtoPoster/poster.py
@@ -16,7 +16,7 @@ def poster_job(context):
         count = 0
         mesaj = update.effective_message.caption
         if len(postee) > 1 and mesaj == None:
-            grup.append(MEDIA_GROUP_TYPES[effective_message_type(update)](media=update.message.photo[-1].file_id if update.message.photo else update.message.effective_attachment.file_id, caption=poste.effective_message.caption))
+            grup.append(MEDIA_GROUP_TYPES[effective_message_type(update)](media=update.effective_message.photo[-1].file_id if update.effective_message.photo else update.effective_message.effective_attachment.file_id, caption=poste.effective_message.caption))
             continue
         if postee.index(poste) != len(postee)-1:
             postee.append(poste)
@@ -268,7 +268,7 @@ def poster_job(context):
                         if len(postee) == 1:
                             post = update.effective_message.copy(kan, caption=sablon)
                         else:
-                            grup.append(MEDIA_GROUP_TYPES[effective_message_type(update)](media=message.photo[-1].file_id if update.message.photo else update.message.effective_attachment.file_id, caption=sablon))
+                            grup.append(MEDIA_GROUP_TYPES[effective_message_type(update)](media=message.photo[-1].file_id if update.effective_message.photo else update.effective_message.effective_attachment.file_id, caption=sablon))
                             post = bot.send_media_group(kan, media=grup)
                     except RetryAfter as rtfr:
                         sleep(rtfr.retry_after+1)
@@ -276,7 +276,7 @@ def poster_job(context):
                             if len(postee) == 1:
                                 post = update.effective_message.copy(kan, caption=sablon)
                             else:
-                                grup.append(MEDIA_GROUP_TYPES[effective_message_type(update)](media=message.photo[-1].file_id if update.message.photo else update.message.effective_attachment.file_id, caption=sablon))
+                                grup.append(MEDIA_GROUP_TYPES[effective_message_type(update)](media=message.photo[-1].file_id if update.effective_message.photo else update.effective_message.effective_attachment.file_id, caption=sablon))
                                 post = bot.send_media_group(kan, media=grup)
                         except Exception as e:
                             if str(e).find("Chat is not found") != -1 or str(e).find("Need administrator") != -1 or str(e).find("bot is not") != -1:
