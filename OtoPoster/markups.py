3eab5449a48c0a4ac075919bb57da31ea14d339c
Pharex <alperenfahrikocakaplan@gmail.com>
2021-08-11 14:33:01 +0300

    Update markups.py

--- OtoPoster/markups.py
+++ OtoPoster/markups.py
@@ -22,7 +22,7 @@ def apimenumark():
     return ReplyKeyboardMarkup(keyboard=[['♻️ API değiştir', '🔗 Site değiştir'], ['🤖 Alternatif Link'], ['↩️ Ana Menü']], resize_keyboard=True, selective=True)
 
 def ekstralarmenumark():
-    return ReplyKeyboardMarkup(keyboard=[['❤️ Beğeni Butonları'], ['📌 Post sabitleme']], resize_keyboard=True, selective=True)
+    return ReplyKeyboardMarkup(keyboard=[['❤️ Beğeni Butonları'], ['📌 Post sabitleme'], ['↩️ Ana Menü']], resize_keyboard=True, selective=True)
 
 def imark():
     imark = ReplyKeyboardMarkup(keyboard=[['❌ İptal']], one_time_keyboard=True, resize_keyboard=True, selective=True)
