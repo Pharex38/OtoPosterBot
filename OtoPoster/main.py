7697d1e77b6e5577ecc7963903816b5bf4051ac4
Pharex <alperenfahrikocakaplan@gmail.com>
2021-08-13 03:06:06 +0300

    Update main.py

--- OtoPoster/main.py
+++ OtoPoster/main.py
@@ -72,7 +72,7 @@ def main() -> None:
             EKSTRAMENU: [MessageHandler(~Filters.command & Filters.update.message, ekstramenu),
             CallbackQueryHandler(begenicall, pattern="^(begeniolustur)$"),
             CallbackQueryHandler(tekrarlisaatayarlacall, pattern="^ts-(.*)")],
-            TSPOST: [MessageHandler(~Filters.command, )],
+            TSPOST: [MessageHandler(~Filters.command, tekrarlipostayarla)],
             TSBASLIK: [MessageHandler(~Filters.command & Filters.text, tekrarlipostbaslikayarla)],
             BEGENI: [MessageHandler(~Filters.command & Filters.text, begenidegistir)],
             APIDEGISTIR: [MessageHandler(~Filters.command & Filters.update.message, apikayit)],
