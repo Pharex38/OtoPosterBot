ab774999ad04b24bebd0554bb0aba648f6c57809
Pharex <alperenfahrikocakaplan@gmail.com>
2021-08-14 20:43:07 +0300

    Update misc.py

--- OtoPoster/misc.py
+++ OtoPoster/misc.py
@@ -129,7 +129,7 @@ def eklentiiletisim(update, context):
     if ileti.split("+")[0].isdigit():
         ileti = ileti.split("+")
         imsgid = context.dispatcher.user_data[int(ileti[0])]['iosmsgid']
-        bot.edit_message_text(str(bot.get_chat(ileti[1]).title)+"\n\n"+ileti[2], ileti[0], imsgid)
+        bot.edit_message_text(str(bot.get_chat(ileti[1]).title)+" "+ileti[2], ileti[0], imsgid)
         return
 
 def comment(update, context):
