5bd8f5525afcf0c97325f2b7d01b61ecea3229ad
Pharex <alperenfahrikocakaplan@gmail.com>
2021-08-16 00:18:25 +0300

    Update TimerEklenti.py

--- TimerEklenti.py
+++ TimerEklenti.py
@@ -36,7 +36,7 @@ def islem(client, message):
             iosrespond = f"<i>Kanalınızda herhangi bir kısıtlama bulunamadı.</i>"
         else:
             for il in list(chat.restrictions):
-                iosrespond += f"Platform: {'iOS' if il['platform'] == 'ios' else il['platform']}\nSebep: {il['reason'].upper()}"
+                iosrespond += f"Platform: {'iOS' if il['platform'] == 'ios' else il['platform']}\nSebep: {il['reason'].upper()}\nKısıtlama: <b>Var</b>"
         return_text = f"{mesaj[0]}+{chat.id}+{iosrespond}"
         message.reply(return_text)
         chat.leave()
