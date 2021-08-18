c76799e8d89282bd5beacb080f7ceab033dfd690
Pharex <alperenfahrikocakaplan@gmail.com>
2021-08-19 02:57:51 +0300

    Update callbacks.py

--- OtoPoster/callbacks.py
+++ OtoPoster/callbacks.py
@@ -245,35 +245,6 @@ def callback_query(call, context):
         bot.edit_message_text("Alternatif olarak kullanmak istediğiniz siteyi seçin.", user, mesajid)
         bot.edit_message_reply_markup(chat_id=chat, message_id=mesajid, reply_markup=altsitemarkup())
     """ Kaynak """
-    if call.callback_query.data == "ozkay":
-        kaynakmsg = call.effective_message
-        kynskm = collection.find_one({"_id": user})['kanal'][0]
-        for m in OzelCol.find({}):
-            if user in m['kanal']:
-                try:
-                    ozel_kaynak_bilgi = bot.get_chat(m['okaynak'])
-                except:
-                    kaynakmsg.edit_text("Botu kaynak kanalınızdan çıkarttığınız için post atılmayacak.", reply_markup=ozelkaynakmark(user, 0))
-                    return
-                kullanan_sayisi = len(m['kanal'])
-                break
-        refsahip = "yok"
-        for ox in OzelCol.find({}):
-            if user in ox['kanal']:
-                refsahip = ox["_id"]
-                break
-        if refsahip == "yok":
-            collection.update_one({"_id": user}, {"$set": {"ozel": False}})
-            collection.update_one({"_id": user}, {"$pull": {"kaynak": "32"}})
-            try:
-                kcisim = bot.get_chat(kynskm).title
-            except:
-                kcisim = "Kanalınıza ulaşılamadı!"
-            kaynakmsg.edit_text(f"""<b> >>>    {kcisim}\n\nKanalınızda kullanmak istediğiniz kaynak kanalını seçin.</b>""", reply_markup=ozelkaynakmark(user, 0))
-            return
-        ref_link = create_deep_linked_url(context.bot.username, str(refsahip))
-        kaynakmsg.edit_text("""<b>Sadece bir tane Özel Kaynak kullanabilirsiniz.</b>\n\n      <i>Özel Kaynağınız:</i><b> <a href="{}">{}</a>\n</b>      <i>Bu Kaynağı Toplam </i><code>{}</code> <i>Kişi Kullanıyor.</i>\n\n<b>Kaynak Referans Linki;</b>\n<code>{}</code>\n<i>Bu link ile botu başlatan herkes otomatik olarak sizin kaynağınıza bağlanacak.</i>""".format(ozel_kaynak_bilgi.invite_link, ozel_kaynak_bilgi.title, kullanan_sayisi, ref_link), reply_markup=ozelkaynakmark(user, 0))
-        return
     if call.callback_query.data == "anakay":
         kaynakmsg = call.effective_message
         kynskm = collection.find_one({"_id": user})['kanal'][0]
