c91de7080844dd44198f4ecde20fe9b20e6018f6
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-08-28 01:44:14 +0300

    update jobs.py

--- OtoPoster/jobs.py
+++ OtoPoster/jobs.py
@@ -158,7 +158,7 @@ def gunluk(context):
     statscount = 1
     stat_text = "👥 Toplam Kullanıcı Sayısı: {}\n📢 Toplam Kayıtlı Kanal Sayısı: {}\n🙋 Toplam Kitle: {}\n\n<b>Sitelerin Toplam Kullanıcı Sayıları(Alternatifler dahil);</b>\nTRLink -> {}\nPND.TL -> {}\nExe.io -> {}\nOuo.io -> {}\nPubiza -> {}\nURLAbly -> {}\nGir.ist -> {}\n\n<b>+18 Kaynakların Toplam Kullanıcı Sayıları:</b>\n".format(users, kanals, toplam, trlink_kullanan_sayisi, pnd_kullanan_sayisi, exe_kullanan_sayisi, ouo_kullanan_sayisi, pubiza_kullanan_sayisi, urlably_kullanan_sayisi, girist)
     gkaynaklar = KaynakCol.find()
-    for kstat in sorted(gkaynaklar, key = lambda i: len(i['kaynak'])):
+    for kstat in sorted(gkaynaklar, key = lambda i: len(i['kaynak']), reverse=True):
         if kstat['icerik'] != "+18":
             continue
         try:
@@ -171,7 +171,7 @@ def gunluk(context):
         statscount += 1
     stat_text += f"Özel Kaynaklar: {ozel_kaynak_kullanan_sayisi}\n\n<b>Arşiv Kaynakların Toplam Kullanıcı Sayıları:</b>\n"
     statscount = 1
-    for kstat in sorted(gkaynaklar, key = lambda i: len(i['kaynak'])):
+    for kstat in sorted(gkaynaklar, key = lambda i: len(i['kaynak']), reverse=True):
         if kstat['icerik'] != "arsiv":
             continue
         try:
