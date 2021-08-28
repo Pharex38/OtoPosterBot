6205c1c92fd5786fd37866fd33e7e344fd4b84d4
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-08-28 09:08:10 +0300

    update jobs.py

--- OtoPoster/jobs.py
+++ OtoPoster/jobs.py
@@ -97,7 +97,6 @@ def gunluk(context):
     kanals = 0
     users = 0
     kullanicilar = collection.find({})
-    """
     for kullanici in kullanicilar:
         try:
             kullanici['site']
@@ -152,7 +151,7 @@ def gunluk(context):
                 else:
                     toplam += uye
                     kanals += 1
-    """
+    
     toplam = toplam / 1000
     toplam = str(round(toplam, 1))+"K" if round(toplam, 1) < 1000 else str(round(toplam / 1000, 2))+"M"
     statscount = 1
@@ -161,7 +160,6 @@ def gunluk(context):
     for kstat in sorted(gkaynaklar, key = lambda i: len(i['kaynak']), reverse=True):
         if kstat['icerik'] != "+18":
             continue
-        bildir(kstat['icerik'])
         try:
             getskaynak = bot.get_chat(kstat['_id'])
         except:
@@ -174,8 +172,7 @@ def gunluk(context):
     statscount = 1
     astat_text = "<b>Arşiv Kaynakların Toplam Kullanıcı Sayıları:</b>\n"
     for kstat in sorted(KaynakCol.find(), key = lambda i: len(i['kaynak']), reverse=True):
-        bildir(kstat['icerik'])
-        if kstat['icerik'] == "+18":
+        if kstat['icerik'] != "arsiv":
             continue
         try:
             getskaynak = bot.get_chat(kstat['_id'])
