ffb67a8a3dbf1c2fc39a1827c2e722a8197ccd02
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-08-30 11:56:16 +0300

    update misc.py

--- OtoPoster/misc.py
+++ OtoPoster/misc.py
@@ -127,6 +127,19 @@ def linkkisalt(site, token, text, icerik):
 
     return link, json
 
+
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
+
 def bildir(neyi='Boş Bildirim Testi !'):
     for i in adminlist:
         try:
