d533ba1100a4642ccd77f968ce09031d0f0a0db8
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-09-01 10:01:41 +0300

    update komutlar.py

--- OtoPoster/komutlar.py
+++ OtoPoster/komutlar.py
@@ -586,7 +586,7 @@ def kaynakpanel(update, context):
     vals = list(panelkaynak['grafik'].values())
     for icc in range(7):
         xlab.append(vals[icc]['user'])
-    pyplot.style.use(['dark_background'])
+    #pyplot.style.use(['dark_background'])
     fig, plot = pyplot.subplots()
     plot.plot(ylab, xlab, label="Kullanıcı Sayısı")
     xlab = []
