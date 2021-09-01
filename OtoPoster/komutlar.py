c0a848668941fee5c4c1a3fbb418ce3c49bdad35
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-09-01 11:50:56 +0300

    update komutlar.py

--- OtoPoster/komutlar.py
+++ OtoPoster/komutlar.py
@@ -589,13 +589,23 @@ def kaynakpanel(update, context):
                         break
     vals = list(panelkaynak['grafik'].values())
     for icc in range(7):
-        xlab.append(vals[icc]['user'])
+        xlab.append(vals[tarihnow.day+icc]['user'])
+    if len(xlab) != 7:
+        for icc in range(7):
+            xlab.append(vals[1+icc]['user'])
+            if len(xlab) == 7:
+                break
     pyplot.style.use(['dark_background'])
     fig, plot = pyplot.subplots()
     plot.bar(ylab, xlab, label="Kullanıcı Sayısı", width=0.8)
     xlab = []
     for icc in range(7):
-        xlab.append(vals[icc]['kanal'])
+        xlab.append(vals[tarihnow.day+icc]['kanal'])
+    if len(xlab) != 7:
+        for icc in range(7):
+            xlab.append(vals[1+icc]['kanal'])
+            if len(xlab) == 7:
+                break
     plot.bar(ylab, xlab, label="Kanal Sayısı", align="edge", width=0.4)
     plot.set_title(panelkaynakkanalisim)
     plot.set_xlabel('7 Günlük Grafik')
