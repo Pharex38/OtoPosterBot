979317a761ad2015177df77cc763308afa236286
Pharex <alperenfahrikocakaplan@gmail.com>
2021-09-07 11:51:13 +0300

    Update misc.py

--- OtoPoster/misc.py
+++ OtoPoster/misc.py
@@ -175,8 +175,8 @@ def kan_mention_html(kanid):
 
 def setup_logger():
     global logger
-    aps_logger = logging.getLogger('apscheduler')
-    aps_logger.setLevel(logging.WARNING)
+    logging.getLogger('apscheduler').setLevel(logging.WARNING)
+    logging.getLogger('matplotlib').setLevel(logging.WARNING)
     zaman = datetime.datetime.now()
     logd = "{}.{}.{} - {}.{}".format(zaman.year, zaman.month, zaman.day, zaman.hour, zaman.minute)
     logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", handlers=[logging.FileHandler(f'Loglar/{logd}.txt', 'w', 'utf-8'), logging.StreamHandler()], level=logging.INFO)
