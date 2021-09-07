bacbd2468524973e76751ed21d4df874d3dd6778
Pharex <alperenfahrikocakaplan@gmail.com>
2021-09-07 21:32:17 +0300

    Update anafonks.py

--- OtoPoster/anafonks.py
+++ OtoPoster/anafonks.py
@@ -350,7 +350,7 @@ def panelbul(update, context):
         update.effective_message.reply_text("Bu kullanıcı sizin kaynağınızı kullanmıyor.")
         return ConversationHandler.END
     bultext = "<b>🔢 ID:</b> {}\n🏷 <b>İsim:</b> {}\n🖥 <b>Kaynağınıza Bağlı Kanalları:</b> \n".format(bul_user, bot.get_chat(bul_user).full_name)
-    bulunanuser = collection.find_one({"kanal": {"$in": [bul_user]}}) if bul_user < 0 else collection.find_one({"_id": bul_user})
+    bulunanuser = collection.find_one({"kanal": {"$in": [str(bul_user)]}}) if bul_user < 0 else collection.find_one({"_id": bul_user})
     for bulkan in bulunanuser['kanal']:
         if not bulkan in KaynakCol.find_one({"sahip": user})['kanal']:
             continue
