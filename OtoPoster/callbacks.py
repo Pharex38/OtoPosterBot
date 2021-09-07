bfe6447eb29af30e129aedfac69c39ce044e7f1c
Pharex <alperenfahrikocakaplan@gmail.com>
2021-09-07 09:56:17 +0300

    Update callbacks.py

--- OtoPoster/callbacks.py
+++ OtoPoster/callbacks.py
@@ -168,7 +168,6 @@ def panelcall(call, context):
     query = call.callback_query
     chat = call.effective_chat.id
     user = call.effective_user.id
-    query.answer("zzzz")
     if query.data == "panelzaman":
         bot.send_message(call.effective_chat.id, "Ayarlamak istediğiniz mesajı gönderin.")
         return PANELZAMAN
