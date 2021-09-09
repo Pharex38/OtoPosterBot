adaac6a61496e114d167b2b7399f4d9ac0098ab9
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-09-09 19:30:43 +0300

    update poster.py

--- OtoPoster/poster.py
+++ OtoPoster/poster.py
@@ -466,7 +466,7 @@ def ozel_poster_job(context):
             except:
                 pass
             else:
-                if ojson['message'] == "Invalid URL":
+                if ojson['message'] != "":
                     logger.error(f"[ÖZEL] {oupdate.effective_message.chat.title} son postu hatalı olduğu için iptal edildi!")
                     try:
                         bot.send_message(sahip, f"{oupdate.effective_message.chat.title} kaynağının sahibi siz olduğunuz için bu mesaj sadece size gönderildi. \n\nSon postunuz kanallarda paylaşılamadı muhtemelen postun linki API ile kısaltılamayacak kadar uzun.\n\n{oupdate.effective_message.link}")
