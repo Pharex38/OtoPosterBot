3ae1a6855c8193f0a1b94bfd22b39b74b4209294
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-09-12 13:48:44 +0300

    update poster.py

--- OtoPoster/poster.py
+++ OtoPoster/poster.py
@@ -109,7 +109,7 @@ def poster_job(context):
             alink = " "
             json = {"shortenedUrl": "", "message": "", "status": ""}
             linktry = 0
-            if site in collection.find_one({"_id": "site"})['site']:
+            if site in collection.find_one({"_id": 0})['site']:
                 logger.warning("Site yasaklı olduğu için atlandı!")
                 continue
             if sira == 2:
