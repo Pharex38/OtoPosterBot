c5f67fd8028ae4323713032361bc910f490c1238
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-09-12 14:15:57 +0300

    update callbacks.py

--- OtoPoster/callbacks.py
+++ OtoPoster/callbacks.py
@@ -680,7 +680,7 @@ def advcall(call, context):
         context.user_data["sss"] = "ekle"
         bot.send_message(chat, "Yeni eklemem istediğiniz siteyi seçin.", reply_markup=altsitemarkup("asite"))
     elif query.data.startswith("advapi-"):
-        query.answer(f"API Adresiniz:\n {advdat['altapi'][int(query.data.split('-')[-1]) ]['site']}", show_alert=True)
+        query.answer(f"API Adresiniz:\n {advdat['altapi'][int(query.data.split('-')[-1]) ]['api']}", show_alert=True)
     
 
 def begeniislemcall(call, context):
