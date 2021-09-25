0389998c371b689b6f95f13418f1e0f3b2cd4f54
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-09-25 23:42:09 +0300

    update callbacks.py

--- OtoPoster/callbacks.py
+++ OtoPoster/callbacks.py
@@ -180,9 +180,9 @@ def panelcall(call, context):
             bot.send_message(chat, "Bulmak istediğiniz kullanıcının ID'sini veya kullanıcıdan herhangi bir mesaj iletin.", reply_markup=imark())
             return PANELBUL
         for paucount in range(que-10,que):
-            if kaynak_users[paucount] == sahip:
-                continue
             try:
+                if kaynak_users[paucount] == sahip:
+                    continue
                 panel_user_text += str(paucount) + ". " + mention_html(kaynak_users[paucount], bot.get_chat(kaynak_users[paucount]).first_name) + "\n"
             except IndexError:
                 paumark = [[InlineKeyboardButton("🔍 Kullanıcı Bul", callback_data="pau-bul")], [InlineKeyboardButton("⏪Önceki Sayfa ", callback_data="pau-{}".format(que-10))]]
