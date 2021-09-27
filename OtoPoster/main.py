3878d25a087f739e41aa9b25399176e224b58610
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-09-27 13:06:19 +0300

    update main.py

--- OtoPoster/main.py
+++ OtoPoster/main.py
@@ -44,6 +44,7 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler('eval', evale, Filters.chat(sahip)))
     dispatcher.add_handler(CommandHandler('poster', posterkomut, Filters.chat(sahip)))
     dispatcher.add_handler(CommandHandler('unban', unbanla, Filters.chat(sahip)))
+    dispatcher.add_handler(CommandHandler('loot', Loot, Filters.chat(sahip)))
     dispatcher.add_handler(CommandHandler('kaynak', yenikaynakkomutu, Filters.chat(sahip)))
     dispatcher.add_handler(CommandHandler('para', parak, Filters.chat(sahip)))
     dispatcher.add_handler(CommandHandler('ban', banla, Filters.chat(sahip)))
