2938da915dd695340e363b9fa5cf22d5b8a4b37d
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-10-03 14:57:38 +0300

    update komutlar.py

--- OtoPoster/komutlar.py
+++ OtoPoster/komutlar.py
@@ -649,7 +649,7 @@ def kaynakpanel(update, context):
     for icc in range(50):
         print(tarihnow.day-2+icc, aykaccekiyo-1)
         if tarihnow.day-2+icc >= aykaccekiyo-1:
-            xlab.append(vals[tarihnow.day-2+icc-aykaccekiyo]['user'])
+            xlab.append(vals[tarihnow.day-1+icc-aykaccekiyo]['user'])
         else:
             try:
                 xlab.append(vals[tarihnow.day-2+icc]['user']) 
