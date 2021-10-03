3eeb89bb0bab6bd5ad23167c6ef83d1838a4c698
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-10-03 14:18:18 +0300

    update komutlar.py

--- OtoPoster/komutlar.py
+++ OtoPoster/komutlar.py
@@ -635,14 +635,14 @@ def kaynakpanel(update, context):
     xlab = []
     for g in dict(panelkaynak['grafik']).keys():
         if int(g) >= tarihnow.day-1 and tarihnow.day-1 < aykaccekiyo:
-            ylab.append(str(g.zfill(2))+"/"+str(tarihnowa.month if int(g) < 15 else tarihnow.month).zfill(2))
+            ylab.append(str(g.zfill(2))+"/"+str(tarihnow.month).zfill(2))
             if len(ylab) == 7:
                 break
     else:
         if len(ylab) != 7:
             for g in dict(panelkaynak['grafik']).keys():
                 if int(g) >= 1:
-                    ylab.append(str(g.zfill(2))+"/"+str(tarihnow.month).zfill(2))
+                    ylab.append(str(g.zfill(2))+"/"+str(tarihnowa.month).zfill(2))
                     if len(ylab) == 7:
                         break
     vals = list(panelkaynak['grafik'].values())
