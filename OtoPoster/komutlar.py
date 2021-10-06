11f735e0cd84be459c2b483617646a5bd3ba95ea
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-10-06 20:50:32 +0300

    update komutlar.py

--- OtoPoster/komutlar.py
+++ OtoPoster/komutlar.py
@@ -541,7 +541,7 @@ def Loot(update, context):
 def evale(update, context):
     user = update.effective_user.id
     try:
-        evol =  eval(update.effective_message.reply_to_message.text.replace("/eval ", "") if update.effective_message.reply_to_message else update.effective_message.text.replace("/eval ", ""))
+        evol =  eval(update.effective_message.text.replace("/eval ", "") if len(update.effective_message.text.split()) > 1 else update.effective_message.reply_to_message.text.replace("/eval ", ""))
     except Exception as ev:
         update.effective_message.reply_text(html.escape(str(ev)))
     else:
