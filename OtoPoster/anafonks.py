3056ce434b743ae79a6560fe7e263a697d51da29
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-10-08 01:28:22 +0300

    update anafonks.py

--- OtoPoster/anafonks.py
+++ OtoPoster/anafonks.py
@@ -539,11 +539,16 @@ def altakayit(update, context):
         collection.update_one({"_id": user}, {"$push": {"altapi": amesaj}})
     else:
         collection.update_one({"_id": user}, {"$set": {"altsite": str(smesaj), "altapi": amesaj, "sira": int(sss)}})
+        seskisablon = collection.find_one({"_id": user})['sablon']
         if int(sss) in [2, 3]:
-            seskisablon = collection.find_one({"_id": user})['sablon']
-            collection.update_one({"_id": user}, {"$set": {"sablon": seskisablon.replace("{alink}", "")}})
+            collection.update_one({"_id": user}, {"$set": {"sablon": seskisablon.replace("{alink}", "∆∆")}})
         else:
-            collection.update_one({"_id": user}, {"$set": {"sablon": "9"}})
+            if "{alink}" in seskisablon:
+                pass
+            elif "∆∆" in seskisablon:
+                collection.update_one({"_id": user}, {"$set": {"sablon": seskisablon.replace("∆∆", "{alink}")}})
+            else:
+                collection.update_one({"_id": user}, {"$set": {"sablon": "9"}})
             
     if sss == 10:
         bot.send_message(chat, "İlk Gelişmiş Alternatifiniz kaydedildi.\n\nAyarlarını görmek için <code>🤖 Alternatif Link</code> butonuna basabilirsin.", reply_markup=apimenumark())
