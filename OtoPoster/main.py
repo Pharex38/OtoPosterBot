0d3b2d9528a26510cbb5ac019408f943882c8713
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-11-05 23:29:34 +0300

    update main.py

--- OtoPoster/main.py
+++ OtoPoster/main.py
@@ -130,8 +130,8 @@ def main() -> None:
     ytjcount = 0
     for uh in collection.find_one({"_id": 0})['jobs']:
         if uh['name'].startswith("ts"):
-            uh['msgdict']['mod'] = "rastgele-0"
             uh['msgdict']['tspost'] = uh['msgdict']
+            uh['msgdict']['mod'] = "rastgele-0"
             upjob.run_repeating(tekrarlipostjob, interval=3600*int(uh['msgdict']['tsaat']), name=uh['name'], context=uh['msgdict'])
             ytjcount += 1
             continue
