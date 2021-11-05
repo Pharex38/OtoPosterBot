2be75eae328a0b8770764bbf5499373df8f5323a
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-11-05 23:22:46 +0300

    update callbacks.py

--- OtoPoster/callbacks.py
+++ OtoPoster/callbacks.py
@@ -704,6 +704,9 @@ def tsmodcall(call, context):
     query = call.callback_query
     try:
         call.effective_message.delete()
+    except:
+        pass
+    context.user_data.clear()
     mod = query.data.split("-")[1] + "-0"
     context.user_data["tspostdict"]["mod"] = mod
     context.job_queue.run_repeating(tekrarlipostjob, first=2, interval=3600*int(context.user_data['tsaat']), name=f"ts{user}", context=context.user_data["tspostdict"])
