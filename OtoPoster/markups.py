8787b81c9e19e440af78d0e679b393d1f1beeda0
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-11-05 21:33:49 +0300

    update markups.py

--- OtoPoster/markups.py
+++ OtoPoster/markups.py
@@ -68,7 +68,7 @@ def tekrarlipostmark(user, context):
     return InlineKeyboardMarkup([[InlineKeyboardButton("Yeni Post Oluştur", callback_data="yenitekrarli")], [InlineKeyboardButton("Tekrarli Post Sil", callback_data="tekrarlisil") if len(context.job_queue.get_jobs_by_name(f"ts{user}")) != 0], [InlineKeyboardButton("❌ İptal", callback_data="iptal")]])
 
 def tspostmod(user):
-    return InlineKeyboardMarkup([[InlineKeyboardButton("Postlardan birini rastgele paylaş.", callback_data="tsmod-rastgele")], [InlineKeyboardButton("Postları sırayla birer birer paylaş.", callback_data="tsmod-sirali")]])
+    return InlineKeyboardMarkup([[InlineKeyboardButton("Postlardan birini rastgele paylaş.", callback_data="tsmod-rastgele-0")], [InlineKeyboardButton("Postları sırayla birer birer paylaş.", callback_data="tsmod-sirali-0")]])
 
 def tekrarlipostsilmark(user, context):
     siltp = []
