bbbb437ab96ea36bfdc151ad17d4a23881dd8047
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-11-06 10:10:57 +0300

    update callbacks.py

--- OtoPoster/callbacks.py
+++ OtoPoster/callbacks.py
@@ -722,7 +722,7 @@ def tsmodcall(call, context):
         pass
     mod = query.data.split("-")[1] + "-0"
     context.user_data["tspostdict"]["mod"] = mod
-    context.job_queue.run_repeating(tekrarlipostjob, first=2, interval=3600*(int(context.user_data['tsaat'])-1), name=f"ts{user}", context=context.user_data["tspostdict"])
+    context.job_queue.run_repeating(tekrarlipostjob, first=2, interval=3600*int(context.user_data['tsaat'])-3600, name=f"ts{user}", context=context.user_data["tspostdict"])
     context.user_data.clear()
     bot.send_message(chat, "Postlarınız başarıyla ayarlandı!", reply_markup=dugme(user))
     return ConversationHandler.END
