835a8b9b5eed1b46badb37d47f54c818d42db411
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-11-07 17:11:15 +0300

    update anafonks.py

--- OtoPoster/anafonks.py
+++ OtoPoster/anafonks.py
@@ -341,7 +341,7 @@ def ekstramenu(update, context):
                     tskanisim bot.get_chat(tspostk).title
                 except:
                     tskanisim = "Kanalınıza ulaşılamadı."
-            text_tekrarli += "<b>Sonraki tetiklenme tarihi:</b> {}\n<b>Paylaşılma aralığı:</b> {}\n<b>Başlık:</b> {}\n<b>Kanal:</b> {}\n<b>Post Sayısı: {}</b>\n\n".format(tpoststr[tpp+8:tpp+27], tpoststr[tapp+4:tapp+13], tpost.context['baslik'], tskanisim, len(tpost.context["tspost"]))
+            text_tekrarli += "<b>Sonraki tetiklenme tarihi:</b> {}\n<b>Paylaşılma aralığı:</b> {}\n<b>Başlık:</b> {}\n<b>Kanal(lar):</b> {}\n<b>Post Sayısı: {}</b>\n\n".format(tpoststr[tpp+8:tpp+27], tpoststr[tapp+4:tapp+13], tpost.context['baslik'], tskanisim, len(tpost.context["tspost"]))
         bot.send_message(chat, text_tekrarli, reply_markup=tekrarlipostmark(user, context))
         return
     if mesaj == "🍎 iOS Ban Kontrol":
