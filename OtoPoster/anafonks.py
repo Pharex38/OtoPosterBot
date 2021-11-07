dd4c95ae7642863e751eda92a62fad53d3d25b4d
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-11-07 17:07:22 +0300

    update anafonks.py

--- OtoPoster/anafonks.py
+++ OtoPoster/anafonks.py
@@ -327,9 +327,14 @@ def ekstramenu(update, context):
             tskanerrorcount = 0
             for tspostk in tpost.context['tskan']:
                 try:
-                    tskanisim = bot.get_chat(tspostk).title
+                    tskanisim += bot.get_chat(tspostk).title + ", " if not tpost.context['tskan'].index(tspostk) in [len(tpost.context['tskan'])-1] and len(tpost.context['tskan']) != 1 else bot.get_chat(tspostk).title
                 except:
                     tskanerrorcount += 1
+            if tskanerrorcount != 0:
+                if len(tpost.context['tskan']) != 1:
+                    tskanisim += " ve {} ulaşılamayan kanal.".format(tskanerrorcount)
+                else:
+                    tskanisim += "Kanalınıza ulaşılamadı.".format(tskanerrorcount)
             text_tekrarli += "<b>Sonraki tetiklenme tarihi:</b> {}\n<b>Paylaşılma aralığı:</b> {}\n<b>Başlık:</b> {}\n<b>Kanal:</b> {}\n<b>Post Sayısı: {}</b>\n\n".format(tpoststr[tpp+8:tpp+27], tpoststr[tapp+4:tapp+13], tpost.context['baslik'], tskanisim, len(tpost.context["tspost"]))
         bot.send_message(chat, text_tekrarli, reply_markup=tekrarlipostmark(user, context))
         return
