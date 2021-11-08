9e6ad8bc4b20d712ed32a6e2d4c865f82abcf995
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-11-09 01:35:20 +0300

    update main.py

--- OtoPoster/main.py
+++ OtoPoster/main.py
@@ -48,7 +48,7 @@ def main() -> None:
     dispatcher.add_handler(AdminCommandHandler('loot', Loot))
     dispatcher.add_handler(AdminCommandHandler('kaynak', yenikaynakkomutu))
     dispatcher.add_handler(AdminCommandHandler('para', parak))
-    dispatcher.add_handler(AdminCommandHandler('ayarlar', AyarlarKomut))
+    dispatcher.add_handler(AdminCommandHandler('ayarlar', AyarlarKomutu))
     dispatcher.add_handler(AdminCommandHandler('ban', banla))
     """ Menü """
     conv_handler = ConversationHandler(
