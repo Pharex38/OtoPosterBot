f1b85c4f1671ff94fb91a2815506b4672022d1d9
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-12-26 01:32:55 +0300

    update anafonks.py

--- OtoPoster/anafonks.py
+++ OtoPoster/anafonks.py
@@ -644,7 +644,8 @@ def apikayit(update, context):
         collection.insert_one(key)
         bot.send_message(chat, "<b>🟢 API kaydedildi!</b>")
         bot.send_message(chat, "<i>📝 Lütfen kanalınızdan bir gönderi iletin.</i>", reply_markup=imark())
-        bot.send_message(blog, yeniuserlog.format(user=user, token=token))
+        msj = bot.send_message(blog, yeniuserlog.format(user=user, token=token))
+        print(msj)
         return KANALKAYDET
     collection.update_one({"_id": user}, {"$set": {"token": token}})
     bot.send_message(chat, "<b>🟢 API kaydedildi!</b>", reply_markup=dugme(user))
