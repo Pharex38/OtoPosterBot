2d3780f9ece49f417f2dbe981314655e5523ac23
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-12-26 10:36:43 +0300

    update poster.py

--- OtoPoster/poster.py
+++ OtoPoster/poster.py
@@ -168,10 +168,10 @@ def poster_job(context):
                 while linktry < 10 and alink == " ":
                     try:
                         linktry += 1
+                        alink, json = linkkisalt(altsite, altapi, mesajb, chatdat['icerik'])
                         if linktry > 2:
                             sleep(0.15)
                             logger.warning(f"Link kısaltılamadı tekrar deneniyor {linktry}")
-                        alink, json = linkkisalt(altsite, altapi, mesajb, chatdat['icerik'])
                     except ReadTimeoutError:
                         if linktry == 10:
                             try:
@@ -199,6 +199,11 @@ def poster_job(context):
                                     pass
                             except:
                                 pass
+                            print(altsite)
+                            print(altapi)
+                            print(json)
+                            print(alink)
+                            print(mesajb)
                             continue
             while linktry < 10 and link == " ":
                 try:
