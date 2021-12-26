c44ff327387b47c661f2dfe6a7ae737512baf3ed
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-12-26 10:46:45 +0300

    update misc.py

--- OtoPoster/misc.py
+++ OtoPoster/misc.py
@@ -125,7 +125,6 @@ def linkkisalt(site, token, text, icerik):
         json = get(f"https://ay.live/api/?", params={'api': token, 'url': text, 'ct': trlinkcat}, headers=headers, timeout=ptimeout).json()
         link = json['shortenedUrl']
     elif site == "2":
-        print(get(f"https://www.pnd.tl/api?", params={'api': token, 'url': text, 'category': pndcat}, headers=headers, timeout=ptimeout).text)
         json = get(f"https://www.pnd.tl/api?", params={'api': token, 'url': text, 'category': pndcat}, headers=headers, timeout=ptimeout).json()
         link = json['shortenedUrl']
     elif site == "3":
