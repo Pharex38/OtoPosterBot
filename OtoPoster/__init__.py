b37a2acb301e91611bea7772315060e205a50eca
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-12-26 11:34:31 +0300

    update __init__.py

--- OtoPoster/__init__.py
+++ OtoPoster/__init__.py
@@ -73,7 +73,7 @@ tips = [
     "Elle Post Paylaşırken post zamanlayabilirsiniz.",
     "Her kanlınıza farklı kaynak seçebilirsiniz."
     ]
-ignorejob = ["yedekleme", "gunluk", "resetleme", "arsivanaposter", "ozelposter", "anaposter", "anapostersiralayici", "ozelpostersiralayici", "panelcleaner"]
+ignorejob = ["yedekleme", "gunluk", "resetleme", "arsivanaposter", "ozelposter", "anaposter", "anapostersiralayici", "ozelpostersiralayici", "panelcleaner", "kisitlamakontrol"]
 ignorekaynak = []
 
 
