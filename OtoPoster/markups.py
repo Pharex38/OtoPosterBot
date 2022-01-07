7feeaf8992b01fee4cd1e2bbccce797ac2e12946
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-07 23:16:10 +0300

    update markups.py

--- OtoPoster/markups.py
+++ OtoPoster/markups.py
@@ -168,7 +168,7 @@ def istekmark(user):
             pass
         else:
             isteklink = "tg://privatepost?channel={}&post=9999999".format(istekkan[3:])
-            istekkeyb.append(InlineKeyboardButton(istekname, url=isteklink))
+            istekkeyb.append([InlineKeyboardButton(istekname, url=isteklink)])
             if istekkan in collection.find_one({"_id": 0})['istek']:
                 isteksatir.append(InlineKeyboardButton("Açık", callback_data="istek-{}".format(istekbutno)))
             else:
