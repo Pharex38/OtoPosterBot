33253c6d7d55aadc8b8e02a7ca78095075115f30
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-08 22:34:40 +0300

    update __init__.py

--- OtoPoster/__init__.py
+++ OtoPoster/__init__.py
@@ -83,6 +83,7 @@ komutisimleri = []
 kansillog = "#KANAL_SİLİNDİ\n_ID: <a href='tg://user?id={user}'>{user}</a>\nÜYE: {membersayi}\nKANAL: <a href='tg://privatepost?channel={kan}&post=9999999'>{kan}</a>\n#kan10{kan}\n#id{user}"
 yenikanlog ="#YENİ_KANAL\n_ID: <a href='tg://user?id={user}'>{user}</a>\nÜYE: {membersayi}\nKANAL: <a href='tg://privatepost?channel={kan}&post=9999999'>{kan}</a>\n#id{user}\n#kan10{kan}"
 yeniuserlog = "#YENİ_KULLANİCİ\n_ID: <a href='tg://user?id={user}'>{user}</a>\nAPI: {token}\n#id{user}\n#api{token}"
+istekonaylog = "#İSTEK_ONAYLANDİ\n_ID: <a href='tg://user?id={user}'>{user}</a>\nİSTEK: {istek}\nKANAL: <a href='tg://privatepost?channel={kan}&post=9999999'>{kan}</a>\n#id{user}\n#kan10{kan}"
 SEND_MEDIA_TYPES = {"document": bot.send_document, "photo": bot.send_photo, "video": bot.send_video, "animation": bot.send_animation}
 POSTMENU, APIMENU, KANALMENU, EKSTRAMENU, TSBASLIK, TSPOST, BEGENI, APIDEGISTIR, KANALKAYDET, SABLONA, PANELZAMAN, PANELBUL, PATPOST, POSTZAMAN, PATZAMAN, CALLALT, ALTAPI, OZELBOTLOG, OZELKAYNAK= range(19)
 headerss = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
