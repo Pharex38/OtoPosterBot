e16e96b08b6331ef14eed550bba9c41f6ac340bc
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-08 12:44:09 +0300

    update markups.py

--- OtoPoster/markups.py
+++ OtoPoster/markups.py
@@ -171,10 +171,10 @@ def istekmark(user):
             isteklink = "tg://privatepost?channel={}&post=9999999".format(istekkan[3:])
             isteksatir.append(InlineKeyboardButton(istekname, url=isteklink))
             if istekkan in collection.find_one({"_id": 0})['istek']:
-                isteksatir2.append(InlineKeyboardButton("Otomatik: Açık", callback_data="istek-{}".format(istekbutno)))
+                isteksatir2.append(InlineKeyboardButton("Oto: ⚫", callback_data="istek-{}".format(istekbutno)))
             else:
-                isteksatir2.append(InlineKeyboardButton("Otomatik: Kapalı", callback_data="istek-{}".format(istekbutno)))
-            isteksatir2.append(InlineKeyboardButton(f"Tümünü Onayla", callback_data="allistek-{}".format(istekbutno)))
+                isteksatir2.append(InlineKeyboardButton("Oto: ✅", callback_data="istek-{}".format(istekbutno)))
+            isteksatir2.append(InlineKeyboardButton(f"♐", callback_data="allistek-{}".format(istekbutno)))
             if len(isteksatir) == 2:
                 istekkeyb.append(isteksatir)
                 istekkeyb.append(isteksatir2)
