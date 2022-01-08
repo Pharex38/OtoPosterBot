ef37e9071a0e0f6140eb1bdd72d70eebb304a987
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-08 13:47:55 +0300

    update callbacks.py

--- OtoPoster/callbacks.py
+++ OtoPoster/callbacks.py
@@ -317,12 +317,12 @@ def callback_query(call, context):
         
         istekkanno = int(call.callback_query.data.split("-")[1])
         istekcount = int(call.callback_query.data.split("-")[2])
-        call.callback_query.edit_message_reply_markup(call.effective_message.text[:-2]+str(istekcount), reply_markup=miktarliistekmark(istekkanno, istekcount))
+        call.callback_query.edit_message_text(call.effective_message.text[:-2]+str(istekcount), reply_markup=miktarliistekmark(istekkanno, istekcount))
         return
     if call.callback_query.data.startswith("allistek-"):
         call.callback_query.answer("ㅤ")
         try:
-            call.callback_query.effective_message.delete()
+            call.effective_message.delete()
         except:
             pass
         istekkanno = int(call.callback_query.data.split("-")[1])
