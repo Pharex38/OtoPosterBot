b4e5dcbd428fdc220ce0e3c043f5a3546ddcbce1
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-09 22:24:56 +0300

    update misc.py

--- OtoPoster/misc.py
+++ OtoPoster/misc.py
@@ -179,6 +179,8 @@ def istekonaylayici(update, context):
         else:
             IstekCol.update_one({"_id": 0}, {"$set": {f"{chat}": {"user": collection.find_one({"kanal": {"$in": [chat]}}).get('_id', sahip), "count": 1, "istekler": []}}})
     else:
+        if IstekCol.find_one({"_id": 0}).get(chat, None) == None:
+            IstekCol.update_one({"_id": 0}, {"$set": {f"{chat}": {"user": collection.find_one({"kanal": {"$in": [chat]}}).get('_id', sahip), "count": 1, "istekler": []}}})
         if not update.chat_join_request.from_user.id in IstekCol.find_one({"_id": 0})[chat]['istekler']:
             IstekCol.update_one({"_id": 0}, {"$push": {f"{chat}.istekler": update.chat_join_request.from_user.id}})
         if not update.chat_join_request.from_user.id in IstekCol.find_one({"_id": 1})['istekler']:
