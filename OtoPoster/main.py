06be83239fb3150354ce4928163f338ed289f3c2
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-12 13:39:41 +0300

    update main.py

--- OtoPoster/main.py
+++ OtoPoster/main.py
@@ -101,7 +101,8 @@ def main() -> None:
         fallbacks=[MessageHandler(Filters.regex('^(↩️ Ana Menü)$') & Filters.update.message, cancel), CommandHandler('start', start, filters=~Filters.update.edited_message)],
         per_message=False,
         name="anaconv",
-        per_chat=True
+        per_chat=True,
+        run_async=False
         )
     dispatcher.add_handler(conv_handler)
     """ Müşteri Komutları """
@@ -127,7 +128,7 @@ def main() -> None:
     dispatcher.add_handler(CallbackQueryHandler(advcall, pattern="^adv(.*)"))
     dispatcher.add_handler(CallbackQueryHandler(callback_query))
     """ Error Handler """
-    dispatcher.add_handler(ChatJoinRequestHandler(istekonaylayici, run_async=False))
+    dispatcher.add_handler(ChatJoinRequestHandler(istekonaylayici))
     dispatcher.add_error_handler(error_handler)
     """ Job Yedekleme """
     yjcount = 0
