a5a2083c241304b8b1a59e0b1c12091f1cecc898
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-14 00:10:34 +0300

    update anafonks.py

--- OtoPoster/anafonks.py
+++ OtoPoster/anafonks.py
@@ -319,9 +319,9 @@ def ekstramenu(update, context):
         bot.send_message(chat, "<b>Paylaşılan postların otomatik olarak sabitlenmesini istersen bu modu açabilirsin.</b>", reply_markup=pinmark(user))
         return
     if mesaj == "📡 İstek Onaylayıcı":
-        
-        bot.send_message(chat, "Bu mod bakımda ve yarına kadar kullanılamayacktır. 🦶😔")
-        return
+        if len(eudat['kanal']) == 0:
+            bot.send_message(chat, "Bu mod bakımda ve yarına kadar kullanılamayacktır. 🦶😔")
+            return
         istektext = ""
         """
         for istekkan in eudat['kanal']:
