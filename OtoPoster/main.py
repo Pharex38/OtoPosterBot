0d6bfc656efeed42a19538b9bc796774101c4b68
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-15 00:47:04 +0300

    update main.py

--- OtoPoster/main.py
+++ OtoPoster/main.py
@@ -18,7 +18,8 @@ def main() -> None:
     dispatcher = updater.dispatcher
     upjob = updater.job_queue
     """ Repeating Jobs """
-
+    tz = get_localzone()
+    print(tz)
     scheduler = BackgroundScheduler()
     scheduler.configure(timezone=tz)
     scheduler.add_job(istekjob, 'interval', seconds=60)
