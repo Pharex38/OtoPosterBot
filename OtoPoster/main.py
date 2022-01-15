6c60d2b71aeca9b8f27112a2dc768a593cffab3c
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-15 09:58:22 +0300

    update main.py

--- OtoPoster/main.py
+++ OtoPoster/main.py
@@ -18,10 +18,6 @@ def main() -> None:
     dispatcher = updater.dispatcher
     upjob = updater.job_queue
     """ Repeating Jobs """
-    scheduler = BackgroundScheduler(timezone=pytz.utc)
-    scheduler.add_job(istekjob, 'interval', seconds=60)
-    scheduler.start()
-    upjob.run_daily(resetleme, time=datetime.datetime.strptime("21-06-30 23:58:00", '%y-%m-%d %H:%M:%S').time(), name="gunluk")
     upjob.run_daily(gunluk, time=datetime.datetime.strptime("21-06-30 21:55:00", '%y-%m-%d %H:%M:%S').time(), name="resetleme")
     upjob.run_repeating(jobyedekleme, interval=300, first=10, name="yedekleme")
     upjob.run_repeating(siraclean, interval=3600, first=10, name="yedekleme")
