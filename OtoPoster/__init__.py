0385e1112d6605b6b347c380e4b160a763d1318a
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-15 10:06:06 +0300

    update __init__.py

--- OtoPoster/__init__.py
+++ OtoPoster/__init__.py
@@ -57,8 +57,7 @@ mainsiralimit = maindata['mainsira']
 
 reqs = Request(con_pool_size=50, connect_timeout=30, read_timeout=30)
 bot = ExtBot(bottoken, request=reqs, defaults=Defaults(parse_mode=ParseMode.HTML, run_async=True, timeout=20, disable_web_page_preview=True, allow_sending_without_reply=True, tzinfo=pytz.timezone('Turkey')))
-pyrobot = pyrogram.Client("pyrobot", bot_token=bottoken, api_id=***REMOVED-API-ID***, api_hash="***REMOVED-API-HASH***")
-pyrobot.start()
+
 eklenti = 1654723447
 blog = -1001391561285
 botlog = -1001352123979
