7e03803fc59dee4a6848c106a879e64f45995265
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-15 15:31:32 +0300

    update callbacks.py

--- OtoPoster/callbacks.py
+++ OtoPoster/callbacks.py
@@ -381,7 +381,7 @@ def callback_query(call, context):
             call.callback_query.edit_message_text("İstek onaylayabilmem için bota kanalınızda <b>Üye Ekleme</b> yetkisi vermelisiniz!")
             return
         if IstekCol.find_one({"_id":0})[pushedistekkan].get('hash', None) == None:
-            bot.send_message(eklenti, f"hash-{istekkanlink.invite_link}")
+            bot.send_message(eklenti, f"hash*{istekkanlink.invite_link}")
         if pushedistekkan in collection.find_one({"_id": 0})['istek']:
             collection.update_one({"_id": 0}, {"$pull": {"istek": pushedistekkan}})
             call.callback_query.answer("Kanalınız için istek modu kapatıldı.")
