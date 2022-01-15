d413a7ed37f536c0b08e66d1870d729dfec237f9
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-16 01:32:49 +0300

    update TimerEklenti.py

--- TimerEklenti.py
+++ TimerEklenti.py
@@ -58,6 +58,8 @@ def islem(client, message):
             return
         print(chato.access_hash)
         IstekCol.update_one({"_id":0}, {"$set": {f"{chatoo.id}.hash": chato.access_hash}})
+        istekler = pyrobot.send(raw.functions.messages.GetChatInviteImporters(peer=kanaloo, limit=10000, offset_date=0, offset_user=raw.types.InputPeerEmpty(), requested=True), retries=1, timeout=10.0, sleep_threshold=5.0)
+        print(istekler.list)
         chato.leave()
 
 def thre():
