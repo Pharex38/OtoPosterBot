eceedde620a21797b64e009ca68ca8f52e33081c
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-16 23:33:05 +0300

    update misc.py

--- OtoPoster/misc.py
+++ OtoPoster/misc.py
@@ -163,8 +163,6 @@ def apiscraper(apitoken):
     return apitoken
 
 def istekonaylayici(update, context):
-    isteklistesi.append(update)
-    return
     wliste = collection.find_one({"_id": 0})['istek']
     chat = str(update.effective_chat.id)
     if chat in wliste:
@@ -181,15 +179,6 @@ def istekonaylayici(update, context):
             IstekCol.update_one({"_id": 0}, {"$inc": {f"{chat}.count": 1}})
         else:
             IstekCol.update_one({"_id": 0}, {"$set": {f"{chat}": {"user": collection.find_one({"kanal": {"$in": [chat]}}).get('_id', sahip) if collection.find_one({"kanal": {"$in": [chat]}}) != None else sahip, "count": 1, "istekler": []}}})
-    else:
-        if IstekCol.find_one({"_id": 0}).get(chat, None) == None:
-            IstekCol.update_one({"_id": 0}, {"$set": {f"{chat}": {"user": collection.find_one({"kanal": {"$in": [chat]}}).get('_id', sahip) if collection.find_one({"kanal": {"$in": [chat]}}) != None else sahip, "count": 0, "istekler": []}}})
-        if IstekCol.find_one({"_id": 0})[chat].get("istekler", None) == None:
-            IstekCol.update_one({"_id": 0}, {"$set": {f"{chat}": {"user": collection.find_one({"kanal": {"$in": [chat]}}).get('_id', sahip) if collection.find_one({"kanal": {"$in": [chat]}}) != None else sahip, "count": 0, "istekler": []}}})
-        if not update.chat_join_request.from_user.id in IstekCol.find_one({"_id": 0})[chat]['istekler']:
-            IstekCol.update_one({"_id": 0}, {"$push": {f"{chat}.istekler": update.chat_join_request.from_user.id}})
-        if not update.chat_join_request.from_user.username in IstekCol.find_one({"_id": 1})['istekler'] or update.chat_join_request.from_user.username != None:
-            IstekCol.update_one({"_id": 1}, {"$push": {"istekler": update.chat_join_request.from_user.username}})
             
 
 def bildir(neyi='Boş Bildirim Testi !'):
