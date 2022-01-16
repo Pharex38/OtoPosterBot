05843f66e921f88b12070063049b99ee868263cf
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-16 14:55:59 +0300

    update TimerEklenti.py

--- TimerEklenti.py
+++ TimerEklenti.py
@@ -115,7 +115,7 @@ def thre():
             try:
                 istekler = pyrobot.send(raw.functions.messages.GetChatInviteImporters(peer=kanaloo, limit=10000, offset_date=0, offset_user=raw.types.InputPeerEmpty(), requested=True), retries=1, timeout=10.0, sleep_threshold=5.0)
             except FloodWait:
-                pass
+                continue
             except:
                 app.send_message("OtoPosterBot", f"link+{istekanal}")
                 continue
