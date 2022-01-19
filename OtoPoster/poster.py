1548292f6ae0a4ab1429a0a3a0e7f156814c8344
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-20 00:18:30 +0300

    update poster.py

--- OtoPoster/poster.py
+++ OtoPoster/poster.py
@@ -180,7 +180,7 @@ def poster_job(context):
                     try:
                         linktry += 1
                         if linktry > 2:
-                            sleep(0.15)
+                            sleep(0.10)
                             logger.warning(f"Link kısaltılamadı tekrar deneniyor {linktry}")
                         alink, ajson = linkkisalt(altsite, altapi, mesajb, chatdat['icerik'])
                     except ReadTimeoutError:
@@ -222,7 +222,7 @@ def poster_job(context):
                 try:
                     linktry += 1
                     if linktry > 2:
-                        sleep(0.15)
+                        sleep(0.10)
                         logger.warning(f"Tekrar deneniyor {linktry}")
                     link, json = linkkisalt(site, token, mesajb, chatdat['icerik'])
                 except ReadTimeoutError:
@@ -313,7 +313,7 @@ def poster_job(context):
                 if not kan in chatdat['kanal'] or kan in eski or chatdat['icerik'] == "arsiv" and not kan in icerik or chatdat['icerik'] == "+18" and kan in icerik:
                     continue
                 post = update.effective_message
-                sleep(0.05)
+                sleep(0.025)
                 try:
                     yetkililer = []
                     for xy in bot.get_chat_administrators(kan, timeout=sendtimeout):
