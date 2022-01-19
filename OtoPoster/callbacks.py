4b1a3d074248992a751c267050cc0c385710c92d
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-20 01:20:07 +0300

    update callbacks.py

--- OtoPoster/callbacks.py
+++ OtoPoster/callbacks.py
@@ -332,7 +332,7 @@ def callback_query(call, context):
         istekkanno = int(call.callback_query.data.split("-")[1])
         istekcount = int(call.callback_query.data.split("-")[2])
         istekkan = collection.find_one({"_id": user})['kanal'][istekkanno]
-        if len(IstekCol.find_one({"_id": 0})[istekkan].get("istekler", 0)) == 0:
+        if len(IstekCol.find_one({"_id": 0})[istekkan].get("istekler", [])) == 0:
             call.callback_query.edit_message_text("Hiç onaylanmamış istek göremiyorum. 😔")
             return
         xrlist = IstekCol.find_one({"_id": 0})[istekkan]["istekler"]
