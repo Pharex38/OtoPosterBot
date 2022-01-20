7d6531d18c17b39198d0502bd22a7cd596824c21
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-01-20 19:51:30 +0300

    Update main.py

--- OtoPoster/main.py
+++ OtoPoster/main.py
@@ -128,7 +128,7 @@ def main() -> None:
     dispatcher.add_handler(CallbackQueryHandler(callback_query))
     """ Error Handler """
     cjrhandler =ChatJoinRequestHandler(istekonaylayici)
-    dispatcher.add_handler(cjrhandler)
+    #dispatcher.add_handler(cjrhandler)
     dispatcher.add_error_handler(error_handler)
     """ Job Yedekleme """
     yjcount = 0
