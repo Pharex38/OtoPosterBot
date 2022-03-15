fbed4a6fe522267a7a17b9e4d55e10cba493ed33
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-03-16 00:02:26 +0300

    update poster.py

--- OtoPoster/poster.py
+++ OtoPoster/poster.py
@@ -292,7 +292,10 @@ def poster_job(context):
                     continue
                 post = update.effective_message
                 try:
-                    yetkililer = [xy.user.id for xy in FloodControl(bot.get_chat_administrators, *[kan]) if xy.can_post_messages or xy.status = "creator"]
+                    yetkililer = []
+                    for xy in FloodControl(bot.get_chat_administrators, *[kan]):
+                        if xy.can_post_messages or xy.status = "creator":
+                            yetkililer.append(xy.user.id)
                 except:
                     continue
                 sleep(0.05)
