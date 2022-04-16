e34ee01acbc5d25b53e4d151784d040ccf93c54c
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-04-16 18:50:23 +0300

    update misc.py

--- OtoPoster/misc.py
+++ OtoPoster/misc.py
@@ -120,7 +120,7 @@ def linkkisalt(site, token, text, icerik):
     'Accept-Encoding': 'gzip, deflate, br',
     'Host': "grimaldis.myguestaccount.com",
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'})
-    sesi.headers = headers
+    sesi.headers['user-agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
     if icerik == "arsiv":
         trlinkcat = 3
         pndcat = 7
