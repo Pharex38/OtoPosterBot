8097c6d02dc193d7a7920c02fe52e93ad74270cd
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-04-16 10:58:18 +0300

    Update __init__.py

--- OtoPoster/__init__.py
+++ OtoPoster/__init__.py
@@ -5,7 +5,7 @@ from requests.exceptions import *
 from time import sleep
 from pymongo import MongoClient
 import time, datetime, calendar, speedtest
-import threading, pytz, os, asyncio, logging, cloudscraper
+import threading, pytz, os, asyncio, logging, cfscrape
 from ssl import CERT_NONE
 from random import choice, randint, shuffle
 from telegram import *
