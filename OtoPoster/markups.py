ad0b6e8d88e977e65848b6b1a2ec126c7d48d74e
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2022-07-08 00:47:15 +0300

    aa

--- OtoPoster/markups.py
+++ OtoPoster/markups.py
@@ -73,8 +73,8 @@ def webappmark(user):
             kobj['no'] = kaynak['no']
             kaynaklistesi.append(kobj)
         wappmark.append([KeyboardButton(text=bot.get_chat(int(wpam)).title, web_app=WebAppInfo(f"https://pharex.dev/otoposter/kaynakmenu/?kanal={wpam[1:]}&user={user}"))])
-    cevap = ReqPost("https://pharex.dev/otoposter/kaynakmenu/", {"data": kaynaklistesi, "user_id": user, "kanal_id": wpam}, headers=headerss)    
-    bildir(cevap)
+    cevap = ReqPost("https://pharex.dev/otoposter/kaynakmenu", {"data": kaynaklistesi, "user_id": user, "kanal_id": wpam}, headers=headerss)    
+    bot.send_message(sahip, str(cevap))
     return ReplyKeyboardMarkup(wappmark, resize_keyboard=True)
 
 def ioskontrolmark(user):
