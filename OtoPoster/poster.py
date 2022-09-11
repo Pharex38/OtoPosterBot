747384c0b720efa113f742926ab1c8458d1678ba
Pharex <alperenfahrikocakaplan@gmail.com>
2022-09-11 17:22:08 +0300

    a

--- OtoPoster/poster.py
+++ OtoPoster/poster.py
@@ -34,17 +34,20 @@ def poster_job(context):
         kynk = FloodControl(bot.get_chat, *[chat])
     mesaj = update.effective_message.caption
     if mesaj == None:
+        logger.warning(f"{kynk.title} - Postta başlık olmadığı için iptal edildi!")
         return
     """  Link tespit  """
     solx = mesaj.rfind("http")
     sol = mesaj.find("http")
     if sol == -1 or sol != solx:
+        logger.warning(f"{kynk.title} - Postta birden fazla link olduğu için iptal edildi!")
         return
     sag = mesaj.find("\n", sol)
     mesajb = mesaj[sol:sag].strip()
     if mesaj.find("\n", sol) == -1:
         mesajb = mesaj[sol:].strip()
     if mesajb.startswith("https://ay") or mesajb.startswith("https://pgg") or mesajb.startswith("https://pnd") or mesajb.startswith("https://ouo") or mesajb.startswith("https://exe") or mesajb.startswith("https://lnk") or mesajb.startswith("https://t.me/"):
+        logger.warning(f"{kynk.title} - Posttaki link zaten kısaltılmış olduğu için iptal edildi!")
         return
     """  Açıklama tespit  """
     ason = mesaj.find("\n")
@@ -240,7 +243,7 @@ def poster_job(context):
                     logger.error(f"{update.effective_message.chat.title} son postu hatalı olduğu için iptal edildi!")
                     FloodControl(bot.send_message, *[sahip, posterrtext.format(update.effective_message.chat.title, json['message'], update.effective_message.link)])
                     FloodControl(bot.send_message, *[chatdat['sahip'], posterrtext.format(update.effective_message.chat.title, json['message'], update.effective_message.link)])
-                      
+
                     context.job_queue.run_once(deljob, when=2, name="yedekleme", context=update)
                     break
             sablondict = {"1": f"🔥{aciklama}\n\n🔱 TIKLA 👉 {link}\n\n📛 SESİ AÇ 'a tıklamayı unutma", "2": f"{aciklama} \n\n         𝙇𝙄𝙉𝙆🔗 {link}\n\n🔔ʙɪʟᴅɪʀɪᴍʟᴇʀɪ ᴀçᴍᴀʏı ᴜɴᴜᴛᴍᴀʏıɴ.\n\n📌 Link Nasıl Açılır Bilmiyorsanız\n\n👉 @TRPNDLinkGecmee", "9": f"{aciklama} \n\n𝙇𝙄𝙉𝙆🔗 {link} \n\n     𝙇𝙄𝙉𝙆🔗 {alink}\n\n 🔔ʙɪʟᴅɪʀɪᴍʟᴇʀɪ ᴀçᴍᴀʏı ᴜɴᴜᴛᴍᴀʏıɴ.\n\n 📌 Link Nasıl Açılır Bilmiyorsanız\n👉 @TRPNDLinkGecmee"}
