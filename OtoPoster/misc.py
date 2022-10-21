e25cb34d5d929045da2c459a44735ff432495f62
Pharex <alperenfahrikocakaplan@gmail.com>
2022-10-21 13:30:40 +0300

    a

--- OtoPoster/misc.py
+++ OtoPoster/misc.py
@@ -134,7 +134,6 @@ def linkkisalt(site, token, text, icerik):
     elif site == "4":
         link = get(f"http://ouo.io/api/{token}?", params={'s': text}, headers=headerss, timeout=ptimeout).text
     elif site == "5":
-        #link = cscraper.get(f"http://pubiza.com/api.php?token={token}&url={text}&ads_type={pubizacat}").text
         link = get(f"http://pubiza.com/api.php?", params={'token': token, 'url': text, 'ads_type': pubizacat}).text
     elif site == "6":
         json = get("http://gir.ist/api?", params={"api": token, "url": text}, headers=headerss, timeout=ptimeout).json()
@@ -173,7 +172,7 @@ def apiscraper(apitoken):
         apitoken = apitoken.split("=")
         apitoken = apitoken[1][:apitoken[1].find("&url")]
     elif "script" in apitoken:
-        apitoken = apitoken.replace(" ", "").replace("\n", "").split("'")
+        apitoken = apitoken.replace(" ", "").replace("\n", "").split("&#x27;")
         apitoken = apitoken[1]
     
         
