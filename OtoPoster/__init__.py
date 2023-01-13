7829821308a43952767b9c392ef265ef45e93e71
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2023-01-14 01:31:50 +0300

    Update __init__.py

--- OtoPoster/__init__.py
+++ OtoPoster/__init__.py
@@ -79,7 +79,7 @@ tips = [
     "Her kanlınıza farklı kaynak seçebilirsiniz."
     ]
 ignorejob = ["yedekleme", "istekjob", "gunluk", "resetleme", "arsivanaposter", "ozelposter", "anaposter", "anapostersiralayici", "ozelpostersiralayici", "panelcleaner", "kisitlamakontrol", "istekonaylayici"]
-ignorekaynak = [20]
+ignorekaynak = [20, 1]
 
 
 komutisimleri = []
